44913a139ee0c3c08b78702cd3a58073c93d1105
tsalm0n <tsalm0n@twc.com>
2019-12-14 18:50:30 -0500

    cleanup

--- sqlArch/save2CSV.js
+++ sqlArch/save2CSV.js
@@ -26,7 +26,7 @@ module.exports = {
 
         const fields = [
             "invPK", "invScanCode", "invName", "ordSupplierStockNumber", "invSize", "invReceiptAlias", "posTimeStamp", "invDateCreated",
-            "invEmpFkCreatedBy", "ordQuantityInOrderUnit", "oupName", "stoName", "brdName", "dptName", "dptNumber", "venCompanyname",
+            "invEmpFkCreatedBy", "ordQuantityInOrderUnit", "oupName", "stoName", "brdName", "dptName", "dptNumber", "sibIdealMargin", "venCompanyname",
             "invLastreceived", "invLastsold", "invLastcost", "sibBasePrice", "invOnhand", "invOnorder", "invIntransit", "pi1Description",
             "pi2Description", "pi3Description", "invPowerField3", "invPowerField4"
         ]
