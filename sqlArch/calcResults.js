e0e6dab7d8d15dc0c6259a480bb9323fc0d2886f
tsalm0n <tsalm0n@twc.com>
2019-12-18 14:34:44 -0500

    only handle items where exist cplt cost different than updated edi cat cost, for WS IMWs

--- sqlArch/calcResults.js
+++ sqlArch/calcResults.js
@@ -374,7 +374,8 @@ module.exports = {
           if (oupNameSplit[0].toLowerCase().includes('ea') && oupNameSplit[0].toLowerCase() !== 'each' ||
             oupNameSplit[0].toLowerCase().includes('cs') && oupNameSplit[0].toLowerCase() !== 'case') {
             if (oupNameSplit[1] !== undefined) {
-              let testCost = (srcRsObj['ediCost'] / oupNameSplit[1]).trim().replace('"', '')
+              let testCost = srcRsObj['ediCost'] / oupNameSplit[1]
+              testCost.trim().replace('"', '')
               console.log(`testCost(${i})==> ${testCost}`)
               console.log(`srcRsObj['cpltCost'](${i})==> ${srcRsObj['cpltCost']}`)
               if (testCost !== srcRsObj['cpltCost'].trim().replace('"', '')) { //only handle items where new edi cat cost not equal to exist. catapult cost
@@ -385,14 +386,16 @@ module.exports = {
             }
           } else {
             if (oupNameVar.trim().toLowerCase() == 'each' || oupNameVar.trim().toLowerCase() == 'ea' || oupNameVar.trim().toLowerCase() == 'case' || oupNameVar.trim().toLowerCase() == 'cs') {
-              let testCost = (srcRsObj['ediCost'] / oupNameSplit[1]).trim().replace('"', '')
+              let testCost = (srcRsObj['ediCost'] / oupNameSplit[1])
+              testCost.trim().replace('"', '')
               if (testCost !== srcRsObj['cpltCost'].trim().replace('"', '')) { //only handle items where new edi cat cost not equal to exist. catapult cost  
                 reviewObj['ediCostMod'] = srcRsObj['ediCostMod'] = srcRsObj['ediCost'] / 1
                 reviewObj['lastCost'] = srcRsObj['lastCost'] = srcRsObj['ediCost'] / 1 //change lastCost to ediCostMod for wholesale IMWs
               }
             } //divide ediCost by 1 for items with oupName value of just "each", "ea", "case", or "cs"
             else {
-              let testCost = (srcRsObj['ediCost'] / oupNameSplit[1]).trim().replace('"', '')
+              let testCost = (srcRsObj['ediCost'] / oupNameSplit[1])
+              testCost.trim().replace('"', '')
               if (testCost !== srcRsObj['cpltCost'].trim().replace('"', '')) { //only handle items where new edi cat cost not equal to exist. catapult cost
                 reviewObj['ediCostMod'] = srcRsObj['ediCostMod'] = srcRsObj['ediCost'] / oupNameVar //divide ediCost by oupName non-parsed value
                 reviewObj['lastCost'] = srcRsObj['lastCost'] = srcRsObj['ediCost'] / oupNameVar //change lastCost to ediCostMod for wholesale IMWs
