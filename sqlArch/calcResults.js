b18f6494744126a93562628cadd797cd53958dde
tsalm0n <tsalm0n@twc.com>
2019-12-18 22:51:13 -0500

    only push items whose exist. cplt cost !== new divided-to-uos edi cost for WS IMWs

--- sqlArch/calcResults.js
+++ sqlArch/calcResults.js
@@ -759,11 +759,26 @@ module.exports = {
 
         if (typeOfIMW.toLowerCase() == 'wholesale') { //start dept filtering handling with wholesale imw,
           //because lower down, we will be filtering for retail imw after running calcCharm()
-
           divideCostToUOS_WS_IMW()
-
-          if (skuOveride.toLowerCase() == 'matchonly') { //option for including or excluding matching catapult/edi SKUs
-            if (nejRows[i][genericHeaderObj.cpltSKUHeader] == nejRows[i][genericHeaderObj.ediSKUHeader]) {
+          if (srcRsObj['ediCostMod'] !== null) { //only push items that have ediCostMod value (which means that exist cplt cost
+            //is different than new divided-to-uos edi cost, as determined in divideCostToUOS_WS_IMW())
+            if (skuOveride.toLowerCase() == 'matchonly') { //option for including or excluding matching catapult/edi SKUs
+              if (nejRows[i][genericHeaderObj.cpltSKUHeader] == nejRows[i][genericHeaderObj.ediSKUHeader]) {
+                srcRsObj['sugstdRtl'] = "" //set sugstdRtl to empty if typeofIMW = 'wholesale'
+                srcRsObj['charm'] = "" //set charm to empty if typeofIMW = 'wholesale'
+                if (deptFilterToApply !== null) { //if a valid dept filter option is entered,
+                  if (srcRsObj['dptNumber'] == deptFilterToApply) { //only push that dept into searchResults
+                    searchResults.push(srcRsObj)
+                    searchResultsForCSV.push(srcRsObj)
+                    searchResultsForCSVreview.push(reviewObj)
+                  }
+                } else { //otherwise, push all depts into searchResults
+                  searchResults.push(srcRsObj)
+                  searchResultsForCSV.push(srcRsObj)
+                  searchResultsForCSVreview.push(reviewObj)
+                }
+              }
+            } else {
               srcRsObj['sugstdRtl'] = "" //set sugstdRtl to empty if typeofIMW = 'wholesale'
               srcRsObj['charm'] = "" //set charm to empty if typeofIMW = 'wholesale'
               if (deptFilterToApply !== null) { //if a valid dept filter option is entered,
@@ -778,23 +793,7 @@ module.exports = {
                 searchResultsForCSVreview.push(reviewObj)
               }
             }
-          } else {
-            srcRsObj['sugstdRtl'] = "" //set sugstdRtl to empty if typeofIMW = 'wholesale'
-            srcRsObj['charm'] = "" //set charm to empty if typeofIMW = 'wholesale'
-            if (deptFilterToApply !== null) { //if a valid dept filter option is entered,
-              if (srcRsObj['dptNumber'] == deptFilterToApply) { //only push that dept into searchResults
-                searchResults.push(srcRsObj)
-                searchResultsForCSV.push(srcRsObj)
-                searchResultsForCSVreview.push(reviewObj)
-              }
-            } else { //otherwise, push all depts into searchResults
-              searchResults.push(srcRsObj)
-              searchResultsForCSV.push(srcRsObj)
-              searchResultsForCSVreview.push(reviewObj)
-            }
           }
-
-
         } else {
           //v//need to run calcCharm for edi catalogs, thus there will be no rb_dept_id key; use value input for globalMargin
           if (formInput0.includes('edi_')) {
