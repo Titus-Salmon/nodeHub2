a35b63ae159473f81bb9a4b7c58f641fb543e017
tsalm0n <tsalm0n@twc.com>
2019-12-30 15:13:45 -0500

    add defaultMarg column to save2CSVreviewNEJ

--- sqlArch/save2CSVreviewNEJ.js
+++ sqlArch/save2CSVreviewNEJ.js
@@ -16,7 +16,7 @@ module.exports = {
 
     const fields = [
       "upc", "cpltSKU", "ediSKU", "name", "oupName", "numPkgs", "csPkgMltpl", "ovr", "stoName", "cpltCost", "ediCost", "ediCostMod", "reqdRetail", "charm",
-      "ediPrice", "sibBasePrice", "dptName", "dptNumber", "sibIdealMargin", "rb_dept_margin", "appldMrgn", "wsDiff_t0d", "discountToApply",
+      "ediPrice", "sibBasePrice", "dptName", "dptNumber", "sibIdealMargin", "defaultMarg", "appldMrgn", "wsDiff_t0d", "discountToApply",
       "edlpVar", "pi1Description", "pi2Description", "pi3Description", "invPowerField3", "invPowerField4"
     ];
     const opts = {
