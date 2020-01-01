00c43b61384dbbbb4384a934e72df31fada29a1a
tsalm0n <tsalm0n@twc.com>
2020-01-01 14:36:30 -0500

    setup rainbow--cat blank table creation & save-to-csv functionality on rb server

--- rainbowCat/rainbowCatDisplay.js
+++ rainbowCat/rainbowCatDisplay.js
@@ -19,6 +19,7 @@ module.exports = {
     function displayrainbowCat(rows) {
       for (let i = 0; i < rows.length; i++) {
         let rainbowCatObj = {}
+        rainbowCatObj['ri_t0d'] = rows[i]['ri_t0d']
         rainbowCatObj['P_K'] = rows[i]['prim_key']
         rainbowCatObj['Vendor'] = rows[i]['vendorName']
         rainbowCatObj['EDI'] = rows[i]['ediName']
