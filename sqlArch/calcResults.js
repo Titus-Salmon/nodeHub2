d7650186d9cc89e9faad644df79f6b54f6e7cbce
tsalm0n <tsalm0n@twc.com>
2020-01-09 16:06:17 -0500

    handle sku mismatch flag option

--- sqlArch/calcResults.js
+++ sqlArch/calcResults.js
@@ -422,8 +422,13 @@ module.exports = {
         function skuMismatchFlagOptionHandler() { //Flag SKU mismatch & leave SKU blank for IMW if skuMismatchFlagOption = "yes"
           if (nejRows[i][genericHeaderObj.cpltSKUHeader] !== nejRows[i][genericHeaderObj.ediSKUHeader]) {
             if (skuMismatchOption == "yes") {
+              console.log(`skuMismatchOption==> ${skuMismatchOption}`)
               srcRsObj['imwSKU'] = reviewObj['imwSKU'] = ""
               srcRsObj['pf4'] = reviewObj['pf4'] = "skuMismatch"
+              console.log(`nejRows[${i}][genericHeaderObj.cpltSKUHeader]==> ${nejRows[i][genericHeaderObj.cpltSKUHeader]}`)
+              console.log(`nejRows[${i}][genericHeaderObj.ediSKUHeader]==> ${nejRows[i][genericHeaderObj.ediSKUHeader]}`)
+              console.log(`srcRsObj['imwSKU']==> ${srcRsObj['imwSKU']}`)
+              console.log(`srcRsObj['pf4']==> ${srcRsObj['pf4']}`)
             }
           }
         }
