724d70ddfb52cb995d67274470066de751a4c43c
tsalm0n <tsalm0n@twc.com>
2020-01-30 11:57:17 -0500

    start building imwGenerator feature

---
+++ imwGen/addNewProducts.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+const mysql = require('mysql')
+
+// const gEnericHdrObj = require('../funcLibT0d/genericHdrObj')
+// const cAlcRsFrmInputs = require('../funcLibT0d/calcResFormInputs')
+
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true //MUST HAVE to make more than 1 sql statement in a single query
+})
+
+module.exports = {
+
+  addNewProducts: router.post('/addNewProducts', (req, res, next) => {
+
+    let imwProductValues = [] //clear searchResults from previous search
+    const postBody = req.body
+    let itemID = postBody['itemIDPost']
+    let suppUnitID = postBody['suppUnitIDPost']
+
+    imwProductValues.push(itemID, suppUnitID)
+
+    res.render('vw-imwGenerator', { //render searchResults to vw-MySqlTableHub page
+      title: `vw-imwGenerator`,
+      imwProductVals: imwProductValues,
+      // loadedSqlTbl: frmInptsObj.loadedSqlTbl,
+      // ongDsc: ongDsc //use to populate value for "%Discount to Apply" field
+    })
+
+  })
+}
---
+++ routes/rt-imwGenerator.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router = express.Router()
+
+//v//destructuring////////////////////////////////
+// const {
+//   ensureAuthenticated
+// } = require('../config/auth-t0dt1tz1')
+
+// const {
+//   createRBtable
+// } = require('../sqlArch/createRBtable')
+// const {
+//   deleteRBtable
+// } = require('../sqlArch/deleteRBtable')
+// const {
+//   populateRBtable
+// } = require('../sqlArch/populateRBtable')
+// const {
+//   queryRBdb
+// } = require('../sqlArch/queryRBdb')
+// const {
+//   nhcrtDisplay
+// } = require('../sqlArch/nhcrtDisplay')
+// const {
+//   nhcrtEdiJoin
+// } = require('../sqlArch/nhcrtEdiJoin')
+// const {
+//   save2CSVnhcrtEdiJoin
+// } = require('../sqlArch/save2CSVnhcrtEdiJoin')
+// const {
+//   save2CSVreviewNEJ
+// } = require('../sqlArch/save2CSVreviewNEJ')
+// const {
+//   saveIMW_CSV
+// } = require('../sqlArch/saveIMW_CSV')
+// const {
+//   loadTable_MySqlHub
+// } = require('../sqlArch/loadTable_MySqlHub')
+// const {
+//   calcResults
+// } = require('../sqlArch/calcResults')
+const {
+  imwGenerator
+} = require('../imwGen/addNewProducts')
+
+
+
+
+router.get('/', function (req, res, next) {
+  res.render('vw-imwGenerator', {
+    title: 'vw-imwGenerator',
+    // username: req.user.name,
+    // userEmail: req.user.email,
+    // userEmail_stringified: JSON.stringify(req.user.email),
+  });
+});
+
+// router.post('/createRBtable', createRBtable)
+// router.post('/deleteRBtable', deleteRBtable)
+// router.post('/populateRBtable', populateRBtable)
+// router.post('/queryRBdb', queryRBdb)
+// router.post('/nhcrtDisplay', nhcrtDisplay)
+// router.post('/nhcrtEdiJoin', nhcrtEdiJoin)
+// router.post('/save2CSVnhcrtEdiJoin', save2CSVnhcrtEdiJoin)
+// router.post('/save2CSVreviewNEJ', save2CSVreviewNEJ)
+// router.post('/saveIMW_CSV', saveIMW_CSV)
+// router.post('/loadTable_MySqlHub', loadTable_MySqlHub)
+// router.post('/calcResults', calcResults)
+router.post('/addNewProducts', addNewProducts)
+
+
+module.exports = router;
