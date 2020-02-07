4ca0672bfd9251f9deeaa674092ca147f3a6e7ac
tsalm0n <tsalm0n@twc.com>
2020-02-07 15:17:19 -0500

    testing JSON format & parsing

--- imwGen/addNewProducts.js
+++ imwGen/addNewProducts.js
@@ -23,95 +23,48 @@ module.exports = {
     var sanitizedItemListAcc
     let objectifiedImwProdArr = []
 
-    // function itemListAccSanitizer() {
-    //   if (itemListAccumulator !== undefined) {
-    //     let sanitizerRegex1 = /(")|(\\)|(\[)|(\])/g
-    //     sanitizedItemListAcc = itemListAccumulator.replace(sanitizerRegex1, "")
-    //     console.log(`sanitizedItemListAcc==> ${sanitizedItemListAcc}`)
-    //   }
-    // }
-
-    function itemListAccSanitizer2() {
+    function itemListAccSanitizer() {
       if (itemListAccumulator !== undefined) {
-        let sanitizerRegex3 = /(\\)|(\[)|(\])/g
-        let sanitizerRegex4 = /("")/g
-        let sanitizerRegex5 = /("{)/g
-        let sanitizerRegex6 = /(}")/g
-        sanitizedItemListAcc = itemListAccumulator.replace(sanitizerRegex3, "")
-          .replace(sanitizerRegex4, `"`).replace(sanitizerRegex5, `{`).replace(sanitizerRegex6, `}`)
+        let sanitizerRegex1 = /(\\)|(\[)|(\])/g
+        let sanitizerRegex2 = /("")/g
+        let sanitizerRegex3 = /("{)/g
+        let sanitizerRegex4 = /(}")/g
+        sanitizedItemListAcc = itemListAccumulator.replace(sanitizerRegex1, "")
+          .replace(sanitizerRegex2, `"`).replace(sanitizerRegex3, `{`).replace(sanitizerRegex4, `}`)
         console.log(`sanitizedItemListAcc==> ${sanitizedItemListAcc}`)
       }
     }
 
-    // function imwProductValObjSanitizer() {
-    //   let sanitizerRegex2 = /(")|(\\)/g
-    //   let stringifiedImwProductValObj = JSON.stringify(imwProductValObj)
-    //   sanitizedImwProductValObj = stringifiedImwProductValObj.replace(sanitizerRegex2, "")
-    //   console.log(`sanitizedImwProductValObj==> ${sanitizedImwProductValObj}`)
-    // }
-
-    // function sanitizedItemListObjGenerator() {
-    //   if (itemListAccumulator !== undefined) {
-    //     itemListAccSanitizer()
-    //     imwProductArr.push(sanitizedItemListAcc)
-    //   }
-    //   imwProductValObj['itemID'] = itemID
-    //   imwProductValObj['suppUnitID'] = suppUnitID
-    //   imwProductValObjSanitizer()
-    //   imwProductArr.push(sanitizedImwProductValObj)
-    //   console.log(`imwProductArr==> ${imwProductArr}`)
-    // }
-
-    // sanitizedItemListObjGenerator()
-
-    function sanitizedItemListObjGenerator2() {
+    function sanitizedItemListObjGenerator() {
       if (itemListAccumulator !== undefined) {
-        itemListAccSanitizer2()
+        itemListAccSanitizer()
         /* X(?=Y) 	Positive lookahead 	X if followed by Y
          * (?<=Y)X 	Positive lookbehind 	X if after Y
          * ==t0d==>you can combine the 2==> (?<=A)X(?=B) to yield: "X if after A and followed by B" <==t0d==*/
         let splitRegex1 = /(?<=}),(?={)/g
         let sanitizedItemListAccSPLIT = sanitizedItemListAcc.split(splitRegex1)
-        console.log(`sanitizedItemListAccSPLIT==> ${sanitizedItemListAccSPLIT}`)
         for (let i = 0; i < sanitizedItemListAccSPLIT.length; i++) {
-          console.log(`sanitizedItemListAccSPLIT[${i}]==> ${sanitizedItemListAccSPLIT[i]}`)
           imwProductArr.push(sanitizedItemListAccSPLIT[i])
         }
       }
       imwProductValObj['itemID'] = itemID
       imwProductValObj['suppUnitID'] = suppUnitID
       let stringifiedImwProductValObj = JSON.stringify(imwProductValObj)
-      console.log(`stringifiedImwProductValObj==> ${stringifiedImwProductValObj}`)
       imwProductArr.push(stringifiedImwProductValObj)
-      console.log(`imwProductArr==> ${imwProductArr}`)
     }
 
-    sanitizedItemListObjGenerator2()
+    sanitizedItemListObjGenerator()
 
     function objectifyImwProductArr() {
-      // let objectifiedImwProdArr = []
       for (let i = 0; i < imwProductArr.length; i++) {
         let objectifiedImwProd = JSON.parse(imwProductArr[i])
         objectifiedImwProdArr.push(objectifiedImwProd)
       }
-      console.log(`objectifiedImwProdArr==> ${objectifiedImwProdArr}`)
     }
 
     objectifyImwProductArr()
 
-    // for (let i = 0; i < imwProductArr.length; i++) {
-    //   console.log(`typeof imwProductArr[${i}]==> ${typeof imwProductArr[i]}`)
-    //   console.log(`imwProductArr[${i}]==> ${imwProductArr[i]}`)
-    //   console.log(`JSON.stringify(imwProductArr[${i}])==> ${JSON.stringify(imwProductArr[i])}`)
-    //   console.log(`imwProductArr[${i}]['itemID']==> ${imwProductArr[i]['itemID']}`)
-    // }
-
-    // let testString = `{"key": "value"}`
-    // let parsedTestString = JSON.parse(testString)
-    // console.log(`parsedTestString==> ${parsedTestString}`)
-    // console.log(`parsedTestString.key==> ${parsedTestString.key}`)
-
-    // console.log(`objectifiedImwProdArr==> ${objectifiedImwProdArr}`)
+    console.log(`typeof imwProductArr==> ${typeof imwProductArr}`)
 
     res.render('vw-imwGenerator', {
       title: `vw-imwGenerator`,
