b637d5a484b587397e9f15fb2f110405ecf607a1
tsalm0n <tsalm0n@twc.com>
2020-02-09 16:37:31 -0500

    begin modularization of imwGen

--- funcLibT0d/itmLstAccSanitizer.js
+++ funcLibT0d/itmLstAccSanitizer.js
@@ -1,23 +1,72 @@
 module.exports = {
-  itmLstAccSntzr: function (postBodyData, ItmLstAcc_obj) {
 
-    // const postBody = req.body
-    let itemID = postBodyData['itemIDPost']
-    let suppUnitID = postBodyData['suppUnitIDPost']
-    ItmLstAcc_obj.ItmLstAcc_obj.itemListAccumulator = postBodyData['ItmLstAcc_obj.itemListAccumulatorPost']
-    let imwProductValObj = {}
-    let imwProductArr = []
-    // var ItmLstAcc_obj.itemListAccumulator
-    let objectifiedImwProdArr = []
 
-    if (ItmLstAcc_obj.itemListAccumulator !== undefined) {
+  itemListObjs: {
+    // itemID: this.itemID,
+    // suppUnitID: this.suppUnitID,
+    // itemListAccumulator: this.itemListAccumulator,
+    // itemID: postBody['itemIDPost'],
+    // suppUnitID: postBody['suppUnitIDPost'],
+    // itemListAccumulator: postBody['itemListAccumulatorPost'],
+    imwProductValObj: {},
+    imwProductArr: [],
+    objectifiedImwProdArr: []
+  },
+
+  itmLstAccSntzr: function (postBodyData) {
+
+    let itemListAccumulator = postBodyData['itemListAccumulatorPost']
+
+    console.log(`itemListAccumulator from itmLstAccSntzr==> ${itemListAccumulator}`)
+
+    if (itemListAccumulator !== undefined) {
       let sanitizerRegex1 = /(\\)|(\[)|(\])/g
       let sanitizerRegex2 = /("")/g
       let sanitizerRegex3 = /("{)/g
       let sanitizerRegex4 = /(}")/g
-      sanitizedItemListAcc = ItmLstAcc_obj.itemListAccumulator.replace(sanitizerRegex1, "")
+      sanitizedItemListAcc = itemListAccumulator.replace(sanitizerRegex1, "")
         .replace(sanitizerRegex2, `"`).replace(sanitizerRegex3, `{`).replace(sanitizerRegex4, `}`)
-      console.log(`sanitizedItemListAcc==> ${sanitizedItemListAcc}`)
+      console.log(`sanitizedItemListAcc from itmLstAccSntzr==> ${sanitizedItemListAcc}`)
+    }
+  },
+
+  sntzdItmLstObjGen: function (postBodyData) {
+
+    let itemID = postBodyData['itemIDPost']
+    let suppUnitID = postBodyData['suppUnitIDPost']
+    let itemListAccumulator = postBodyData['itemListAccumulatorPost']
+    console.log(`itemID from sntzdItmLstObjGen1==> ${itemID}`)
+
+    console.log(`itemListAccumulator from sntzdItmLstObjGen==> ${itemListAccumulator}`)
+
+    if (itemListAccumulator !== undefined) {
+      console.log(`itemID from sntzdItmLstObjGen2==> ${itemID}`)
+      module.exports.itmLstAccSntzr(postBodyData)
+      /* X(?=Y) 	Positive lookahead 	X if followed by Y
+       * (?<=Y)X 	Positive lookbehind 	X if after Y
+       * ==t0d==>you can combine the 2==> (?<=A)X(?=B) to yield: "X if after A and followed by B" <==t0d==*/
+      let splitRegex1 = /(?<=}),(?={)/g
+      console.log(`sanitizedItemListAcc from sntzdItmLstObjGen==> ${sanitizedItemListAcc}`)
+      let sanitizedItemListAccSPLIT = sanitizedItemListAcc.split(splitRegex1)
+      for (let i = 0; i < sanitizedItemListAccSPLIT.length; i++) {
+        module.exports.itemListObjs.imwProductArr.push(sanitizedItemListAccSPLIT[i])
+      }
+    }
+    console.log(`module.exports.itemListObjs.imwProductArr from sntzdItmLstObjGen==> ${module.exports.itemListObjs.imwProductArr}`)
+    module.exports.itemListObjs.imwProductValObj['itemID'] = itemID
+    module.exports.itemListObjs.imwProductValObj['suppUnitID'] = suppUnitID
+    let stringifiedImwProductValObj = JSON.stringify(module.exports.itemListObjs.imwProductValObj)
+    module.exports.itemListObjs.imwProductArr.push(stringifiedImwProductValObj)
+  },
+
+  objctfyImwPrdctArr: function () {
+    // let objectifiedImwProdArr = []
+    for (let i = 0; i < module.exports.itemListObjs.imwProductArr.length; i++) {
+      let objectifiedImwProd = JSON.parse(module.exports.itemListObjs.imwProductArr[i])
+      module.exports.itemListObjs.objectifiedImwProdArr.push(objectifiedImwProd)
+      console.log(`JSON.stringify(module.exports.itemListObjs.objectifiedImwProdArr)==> ${JSON.stringify(module.exports.itemListObjs.objectifiedImwProdArr)}`)
     }
   }
+
+
 }
--- imwGen/addNewProducts.js
+++ imwGen/addNewProducts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const itmlstaccsntzr = require('../funcLibT0d/itmLstAccSanitizer')
+// const itmlstaccsntzr = require('../funcLibT0d/itmLstAccSanitizer')
 // const mysql = require('mysql')
 
 // const connection = mysql.createConnection({
@@ -16,65 +16,73 @@ module.exports = {
 
   addNewProducts: router.post('/addNewProducts', (req, res, next) => {
 
+    const itmlstaccsntzr = require('../funcLibT0d/itmLstAccSanitizer')
+
     const postBody = req.body
-    let itemID = postBody['itemIDPost']
-    let suppUnitID = postBody['suppUnitIDPost']
-    let itemListAccumulator = postBody['itemListAccumulatorPost']
-    let imwProductValObj = {}
-    let imwProductArr = []
-    var sanitizedItemListAcc
-    let objectifiedImwProdArr = []
+    // let itemID = postBody['itemIDPost']
+    // let suppUnitID = postBody['suppUnitIDPost']
+    // // let itemListAccumulator = postBody['itemListAccumulatorPost']
+    // let imwProductValObj = {}
+    // let imwProductArr = []
+    // var sanitizedItemListAcc
+    // let objectifiedImwProdArr = []
 
-    function itemListAccSanitizer() {
-      if (itemListAccumulator !== undefined) {
-        let sanitizerRegex1 = /(\\)|(\[)|(\])/g
-        let sanitizerRegex2 = /("")/g
-        let sanitizerRegex3 = /("{)/g
-        let sanitizerRegex4 = /(}")/g
-        sanitizedItemListAcc = itemListAccumulator.replace(sanitizerRegex1, "")
-          .replace(sanitizerRegex2, `"`).replace(sanitizerRegex3, `{`).replace(sanitizerRegex4, `}`)
-        console.log(`sanitizedItemListAcc==> ${sanitizedItemListAcc}`)
-      }
-    }
+    // let ItmLstAcc_obj = {}
 
-    function sanitizedItemListObjGenerator() {
-      if (itemListAccumulator !== undefined) {
-        itemListAccSanitizer()
-        /* X(?=Y) 	Positive lookahead 	X if followed by Y
-         * (?<=Y)X 	Positive lookbehind 	X if after Y
-         * ==t0d==>you can combine the 2==> (?<=A)X(?=B) to yield: "X if after A and followed by B" <==t0d==*/
-        let splitRegex1 = /(?<=}),(?={)/g
-        let sanitizedItemListAccSPLIT = sanitizedItemListAcc.split(splitRegex1)
-        for (let i = 0; i < sanitizedItemListAccSPLIT.length; i++) {
-          imwProductArr.push(sanitizedItemListAccSPLIT[i])
-        }
-      }
-      imwProductValObj['itemID'] = itemID
-      imwProductValObj['suppUnitID'] = suppUnitID
-      let stringifiedImwProductValObj = JSON.stringify(imwProductValObj)
-      imwProductArr.push(stringifiedImwProductValObj)
-    }
+    // itmlstaccsntzr.itmLstAccSntzr(postBody)
 
-    sanitizedItemListObjGenerator()
+    // function itemListAccSanitizer() {
+    //   if (itemListAccumulator !== undefined) {
+    //     let sanitizerRegex1 = /(\\)|(\[)|(\])/g
+    //     let sanitizerRegex2 = /("")/g
+    //     let sanitizerRegex3 = /("{)/g
+    //     let sanitizerRegex4 = /(}")/g
+    //     sanitizedItemListAcc = itemListAccumulator.replace(sanitizerRegex1, "")
+    //       .replace(sanitizerRegex2, `"`).replace(sanitizerRegex3, `{`).replace(sanitizerRegex4, `}`)
+    //     console.log(`sanitizedItemListAcc==> ${sanitizedItemListAcc}`)
+    //   }
+    // }
 
-    function objectifyImwProductArr() {
-      for (let i = 0; i < imwProductArr.length; i++) {
-        let objectifiedImwProd = JSON.parse(imwProductArr[i])
-        objectifiedImwProdArr.push(objectifiedImwProd)
-      }
-    }
+    // function sanitizedItemListObjGenerator() {
+    //   if (itemListAccumulator !== undefined) {
+    //     itemListAccSanitizer()
+    //     /* X(?=Y) 	Positive lookahead 	X if followed by Y
+    //      * (?<=Y)X 	Positive lookbehind 	X if after Y
+    //      * ==t0d==>you can combine the 2==> (?<=A)X(?=B) to yield: "X if after A and followed by B" <==t0d==*/
+    //     let splitRegex1 = /(?<=}),(?={)/g
+    //     let sanitizedItemListAccSPLIT = sanitizedItemListAcc.split(splitRegex1)
+    //     for (let i = 0; i < sanitizedItemListAccSPLIT.length; i++) {
+    //       imwProductArr.push(sanitizedItemListAccSPLIT[i])
+    //     }
+    //   }
+    //   imwProductValObj['itemID'] = itemID
+    //   imwProductValObj['suppUnitID'] = suppUnitID
+    //   let stringifiedImwProductValObj = JSON.stringify(imwProductValObj)
+    //   imwProductArr.push(stringifiedImwProductValObj)
+    // }
 
-    objectifyImwProductArr()
+    // sanitizedItemListObjGenerator()
+    itmlstaccsntzr.sntzdItmLstObjGen(postBody)
 
-    console.log(`typeof imwProductArr==> ${typeof imwProductArr}`)
-    console.log(`imwProductArr==> ${imwProductArr}`)
-    console.log(`typeof imwProductArr[0]==> ${typeof imwProductArr[0]}`)
+    // function objectifyImwProductArr() {
+    //   for (let i = 0; i < imwProductArr.length; i++) {
+    //     let objectifiedImwProd = JSON.parse(imwProductArr[i])
+    //     objectifiedImwProdArr.push(objectifiedImwProd)
+    //   }
+    // }
+
+    // objectifyImwProductArr()
+    itmlstaccsntzr.objctfyImwPrdctArr()
+
+    // console.log(`typeof imwProductArr==> ${typeof imwProductArr}`)
+    // console.log(`imwProductArr==> ${imwProductArr}`)
+    // console.log(`typeof imwProductArr[0]==> ${typeof imwProductArr[0]}`)
 
     res.render('vw-imwGenerator', {
       title: `vw-imwGenerator`,
-      imwProductValObj: imwProductValObj,
-      imwProductArr: imwProductArr,
-      objectifiedImwProdArr: objectifiedImwProdArr
+      imwProductValObj_t0d: itmlstaccsntzr.itemListObjs.imwProductValObj,
+      imwProductArr_t0d: itmlstaccsntzr.itemListObjs.imwProductArr,
+      objectifiedImwProdArr_t0d: itmlstaccsntzr.itemListObjs.objectifiedImwProdArr
     })
 
   })
