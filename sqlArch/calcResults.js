f4bfece53f3a5d22531f44f6d124e7d36254b5aa
tsalm0n <tsalm0n@twc.com>
2020-03-02 13:46:59 -0500

    paginate frontend while delivering non-paginated calcResults for CSV creation

--- sqlArch/calcResults.js
+++ sqlArch/calcResults.js
@@ -19,42 +19,13 @@ module.exports = {
 
   calcResults: router.post('/calcResults', (req, res, next) => {
 
-    // function baseQuery() {
-    //   connection.query( //1st query is pagination query; 2nd query is getting EDLP data; 3rd query is non-paginated query
-    //     `SELECT * FROM ${frmInptsObj.formInput0} GROUP BY ${genericHeaderObj.upcHeader},
-    //   ${genericHeaderObj.invLastcostHeader} ORDER BY ${genericHeaderObj.upcHeader} 
-    //   LIMIT ${paginPostObj['offsetPost']},${paginPostObj['numQueryRes']};
-
-    //   SELECT * FROM rb_edlp_data;
-
-    //   SELECT * FROM ${frmInptsObj.formInput0} GROUP BY ${genericHeaderObj.upcHeader},
-    //   ${genericHeaderObj.invLastcostHeader} ORDER BY ${genericHeaderObj.upcHeader};`,
-
-    //     function (err, rows, fields) {
-    //       if (err) throw err
-
-    //       console.log(`rows.length from baseQuery==> ${rows.length}`)
-
-    //       let nejRowsPagin = rows[0] //targets 1st query on NEJ table
-    //       let edlpRows = rows[1] //targets 2nd query on rb_edlp_data table
-    //       let nejRowsNonPagin = rows[2] //targets 3rd query on NEJ table
-    //     })
-    // }
-
-    // let nejRowsPagin = rows[0] //targets 1st query on NEJ table
-    // let edlpRows = rows[1] //targets 2nd query on rb_edlp_data table
-    // let nejRowsNonPagin = rows[2] //targets 3rd query on NEJ table
-
-    // let searchResults = [] //clear searchResults from previous search
-    let searchResultsNonPag = []
-    let searchResultsPag = []
-    srcRsCSV_Pag = []
-    srcRsCSV_nonPag = []
-    // searchResultsForCSV = []
-    srcRsCSVrvwPag = []
-    srcRsCSVrvw_nonPag = []
-    // searchResultsForCSVreview = [] //this is for holding data to generate your review excel sheet for Andrea & Brad/Nathan
-    csvContainer = []
+    let searchResultsNonPag = [] //clear searchResultsNonPag from previous search
+    let searchResultsPag = [] //clear searchResultsPag from previous search
+    srcRsCSV_Pag = [] //why does this break if instantiated with "let"? A question for another time...
+    srcRsCSV_nonPag = [] //why does this break if instantiated with "let"? A question for another time...
+    srcRsCSVrvwPag = [] //why does this break if instantiated with "let"? A question for another time...
+    srcRsCSVrvw_nonPag = [] //why does this break if instantiated with "let"? A question for another time...
+    csvContainer = [] //why does this break if instantiated with "let"? A question for another time...
     const postBody = req.body
 
     let frmInptsObj = {} //provide empty object to populate with form inputs & values generated from calcResFormInputs.js module
@@ -67,28 +38,28 @@ module.exports = {
     let paginPostObj = {}
     paginPost.paginPost(postBody, paginPostObj)
 
-    let paginPostOption = postBody['paginPostOptionPost']
+    // let paginPostOption = postBody['paginPostOptionPost']
 
-    function queryNhcrtEdiJoinTable() {
-      //v//retrieve info from database table to display in DOM table/////////////////////////////////////////////////////////
-      //filters by UPC & catapult cost (want to grab any differing cost items & make decision on what to do in showSearchResults())
-      connection.query(`SELECT * FROM ${frmInptsObj.formInput0} GROUP BY ${genericHeaderObj.upcHeader},
-      ${genericHeaderObj.invLastcostHeader} ORDER BY ${genericHeaderObj.upcHeader};
-      SELECT * FROM rb_edlp_data;`,
-        function (err, rows, fields) {
-          if (err) throw err
+    // function queryNhcrtEdiJoinTable() {
+    //   //v//retrieve info from database table to display in DOM table/////////////////////////////////////////////////////////
+    //   //filters by UPC & catapult cost (want to grab any differing cost items & make decision on what to do in showSearchResults())
+    //   connection.query(`SELECT * FROM ${frmInptsObj.formInput0} GROUP BY ${genericHeaderObj.upcHeader},
+    //   ${genericHeaderObj.invLastcostHeader} ORDER BY ${genericHeaderObj.upcHeader};
+    //   SELECT * FROM rb_edlp_data;`,
+    //     function (err, rows, fields) {
+    //       if (err) throw err
 
-          showSearchResults.showSearchResults(rows, genericHeaderObj, frmInptsObj, searchResults, searchResultsForCSVreview)
+    //       showSearchResults.showSearchResults(rows, genericHeaderObj, frmInptsObj, searchResults, searchResultsForCSVreview)
 
-          res.render('vw-MySqlTableHub', { //render searchResults to vw-MySqlTableHub page
-            title: `Retail Price Calculator (using nhcrtEdiJoin table: <<${frmInptsObj.loadedSqlTbl}>>)`,
-            searchResRows: searchResults,
-            loadedSqlTbl: frmInptsObj.loadedSqlTbl,
-            // ongDsc: ongDsc //use to populate value for "%Discount to Apply" field
-          })
-        })
+    //       res.render('vw-MySqlTableHub', { //render searchResults to vw-MySqlTableHub page
+    //         title: `Retail Price Calculator (using nhcrtEdiJoin table: <<${frmInptsObj.loadedSqlTbl}>>)`,
+    //         searchResRows: searchResults,
+    //         loadedSqlTbl: frmInptsObj.loadedSqlTbl,
+    //         // ongDsc: ongDsc //use to populate value for "%Discount to Apply" field
+    //       })
+    //     })
 
-    }
+    // }
 
     function queryNejTablePaginated() {
       //v//retrieve info from database table to display in DOM table/////////////////////////////////////////////////////////
@@ -126,12 +97,13 @@ module.exports = {
           })
         })
     }
-    if (paginPostOption == 'no') { //if we choose to paginate, we don't return all results in scrRsObj, and therefore can't generate
-      //a complete IMW or Retail Review. Therefore, YOU MUST CHOOSE NOT TO PAGINATE WHEN GENERATING IMW OR RETAIL REVIEW
-      queryNhcrtEdiJoinTable() //returns all results of srcRsObj (use for IMW & Rtl Rvw)
-    } else {
-      queryNejTablePaginated() //returns only part of srcRsObj, determined by pagination settings. DO NOT USE FOR GENERATING
-      //COMPLETE IMWs or complete Retail Reviews, although CAN be used to generate partial IMWs or partial Retail Reviews
-    }
+    // if (paginPostOption == 'no') { //if we choose to paginate, we don't return all results in scrRsObj, and therefore can't generate
+    //   //a complete IMW or Retail Review. Therefore, YOU MUST CHOOSE NOT TO PAGINATE WHEN GENERATING IMW OR RETAIL REVIEW
+    //   queryNhcrtEdiJoinTable() //returns all results of srcRsObj (use for IMW & Rtl Rvw)
+    // } else {
+    //   queryNejTablePaginated() //returns only part of srcRsObj, determined by pagination settings. DO NOT USE FOR GENERATING
+    //   //COMPLETE IMWs or complete Retail Reviews, although CAN be used to generate partial IMWs or partial Retail Reviews
+    // }
+    queryNejTablePaginated()
   })
 }
