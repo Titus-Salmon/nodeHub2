f57cafef963086ae3eedcf68bd40af386b4ea40e
tsalm0n <tsalm0n@twc.com>
2020-03-03 23:42:43 -0500

    switch to backend caching to solve formInputsObj & genericHeaderObj persistence

--- routes/rt-MySqlTableHub.js
+++ routes/rt-MySqlTableHub.js
@@ -39,9 +39,9 @@ const {
 const {
   calcResults
 } = require('../sqlArch/calcResults')
-const {
-  calcResultsGET
-} = require('../sqlArch/calcResultsGET')
+// const {
+//   calcResultsGET
+// } = require('../sqlArch/calcResultsGET')
 
 
 
@@ -66,7 +66,7 @@ router.post('/save2CSVreviewNEJ', save2CSVreviewNEJ)
 router.post('/saveIMW_CSV', saveIMW_CSV)
 router.post('/loadTable_MySqlHub', loadTable_MySqlHub)
 router.post('/calcResults', calcResults)
-router.get('/calcResults', calcResultsGET)
+router.get('/calcResults', calcResults)
 
 
 module.exports = router;
--- sqlArch/calcResults.js
+++ sqlArch/calcResults.js
@@ -7,6 +7,10 @@ const cAlcRsFrmInputs = require('../funcLibT0d/calcResFormInputs')
 const paginPost = require('../funcLibT0d/paginPost')
 const showSearchResults = require('../funcLibT0d/showSearchResults')
 
+const NodeCache = require('node-cache')
+const formInputsObjCache = new NodeCache()
+const genericHeaderObjCache = new NodeCache()
+
 const connection = mysql.createConnection({
   host: process.env.RB_HOST,
   user: process.env.RB_USER,
@@ -28,11 +32,27 @@ module.exports = {
     csvContainer = [] //why does this break if instantiated with "let"? A question for another time...
     const postBody = req.body
 
+    // const formInputsObjCache = new NodeCache()
+    // const genericHeaderObjCache = new NodeCache()
+
+    formInputsObjCacheChecker = formInputsObjCache.get('formInputsObjCache_key');
+    if (formInputsObjCacheChecker !== undefined) {
+      formInputsObjCache.del('formInputsObjCache_key')
+    }
+    genericHeaderObjCacheChecker = genericHeaderObjCache.get('genericHeaderObjCache_key');
+    if (genericHeaderObjCacheChecker !== undefined) {
+      genericHeaderObjCache.del('genericHeaderObjCache_key')
+    }
+
     let frmInptsObj = {} //provide empty object to populate with form inputs & values generated from calcResFormInputs.js module
     cAlcRsFrmInputs.clcRsFrmInpts(postBody, frmInptsObj)
+    // const formInputsObjCache = new NodeCache()
+    formInputsObjCache.set('formInputsObjCache_key', frmInptsObj)
 
     let genericHeaderObj = {} //provide empty object to populate with generic headers generated from genericHdrObj.js module
     gEnericHdrObj.gnrcHdrObj(postBody, genericHeaderObj)
+    // const genericHeaderObjCache = new NodeCache()
+    genericHeaderObjCache.set('genericHeaderObjCache_key', genericHeaderObj)
 
     let pageLinkArray = []
     let numPagesPlaceholder = []
@@ -111,5 +131,117 @@ module.exports = {
         })
     }
     queryNejTablePaginated()
+  }),
+
+
+  calcResultsGET: router.get('/calcResults', (req, res, next) => {
+
+    console.log(`formInputsObjCacheChecker from GET==> ${formInputsObjCacheChecker}`)
+    console.log(`genericHeaderObjCacheChecker from GET==> ${genericHeaderObjCacheChecker}`)
+
+    let searchResultsPagGETarr = [] //clear searchResultsPag from previous search
+    srcRsCSV_PagGETarr = []
+    srcRsCSVrvwPagGETarr = []
+
+    console.log(`req.query==> ${req.query}`)
+
+    console.log(`encodeURIComponent(req.query)==> ${decodeURIComponent(req.query)}`)
+
+    console.log(`JSON.stringify(req.query)==> ${JSON.stringify(req.query)}`)
+    console.log(`req.query.page==> ${req.query.page}`)
+    console.log(`req.query.tableName==> ${req.query.tableName}`)
+    console.log(`req.query.numQueryRes==> ${req.query.numQueryRes}`)
+
+    let page = parseInt(decodeURIComponent(req.query.page))
+    let tableName = decodeURIComponent(req.query.tableName)
+    let numQueryRes = decodeURIComponent(req.query.numQueryRes)
+
+    let offset = page * numQueryRes
+
+    // let upcHeaderGET = decodeURIComponent(req.query.upcHeader)
+    // let invLastcostHeaderGET = decodeURIComponent(req.query.invLastcostHeader)
+
+    console.log(`req.query.formInputsObj_stringified==> ${req.query.formInputsObj_stringified}`)
+    let formInputsObjGET = JSON.parse(decodeURIComponent(req.query.formInputsObj_stringified))
+    console.log(`formInputsObjGET==> ${formInputsObjGET}`)
+    let genericHeaderObjGET = JSON.parse(decodeURIComponent(req.query.genericHeaderObj_stringified))
+    console.log(`genericHeaderObjGET==> ${genericHeaderObjGET}`)
+    console.log(`JSON.stringify(genericHeaderObjGET)==> ${JSON.stringify(genericHeaderObjGET)}`)
+    console.log(`genericHeaderObjGET.upcHeader==> ${genericHeaderObjGET.upcHeader}`)
+
+    // let searchResultsPagGET = decodeURIComponent(req.query.searchResultsPag_stringified)
+    // let searchResultsPagGETarr = searchResultsPagGET.split(',')
+    // console.log(`searchResultsPagGETarr==> ${searchResultsPagGETarr}`)
+    // let srcRsCSV_PagGET = decodeURIComponent(req.query.srcRsCSV_Pag_stringified)
+    // let srcRsCSV_PagGETarr = srcRsCSV_PagGET.split(',')
+    // let srcRsCSVrvwPagGET = decodeURIComponent(req.query.srcRsCSVrvwPag_stringified)
+    // let srcRsCSVrvwPagGETarr = srcRsCSVrvwPagGET.split(',')
+
+    let paginPostObjGET = decodeURIComponent(req.query.paginPostObj)
+
+    // let imwProductValObj = decodeURIComponent(req.query.imwProductValObj)
+    // let imwProductArr = decodeURIComponent(req.query.imwProductArr)
+
+    // console.log(`imwProductArr from GET==> ${imwProductArr}`)
+    // console.log(`typeof imwProductArr from GET==> ${typeof imwProductArr}`)
+    // console.log(`JSON.stringify(imwProductArr) from GET==> ${JSON.stringify(imwProductArr)}`)
+
+    let pageLinkArray = []
+    let numPagesPlaceholder = [] //holds the value for total number of pages; should only be one value
+    // let srsObjArr = []
+
+    let currentPage = page
+    console.log(`currentPage from GET==> ${currentPage}`)
+
+    function queryEDI_Table_GET() {
+      //The COUNT(*) function returns the number of rows in a result set returned by a SELECT statement.
+      //The COUNT(*) returns the number of rows including duplicate, non-NULL and NULL rows.
+      connection.query( //1st query is pagination query; 2nd query is getting EDLP data; 3rd query is non-paginated query;
+        //4th query is for getting COUNT (# of total rows)
+        `SELECT * FROM ${tableName} GROUP BY ${genericHeaderObjGET.upcHeader},
+      ${genericHeaderObjGET.invLastcostHeader} ORDER BY ${genericHeaderObjGET.upcHeader} 
+      LIMIT ${offset},${numQueryRes};
+
+      SELECT * FROM rb_edlp_data;
+      
+      SELECT * FROM ${tableName} GROUP BY ${genericHeaderObjGET.upcHeader},
+      ${genericHeaderObjGET.invLastcostHeader} ORDER BY ${genericHeaderObjGET.upcHeader};
+      
+      SELECT COUNT(*) FROM ${tableName};`,
+        function (err, rows, fields) {
+          if (err) throw err
+
+          let nejRowsPagin = rows[0] //targets 1st query on NEJ table
+          let edlpRows = rows[1] //targets 2nd query on rb_edlp_data table
+          let nejRowsNonPagin = rows[2] //targets 3rd query on NEJ table
+
+          let countRows = rows[3]
+
+          // showSearchRes.showSearchRes(rows, numQueryRes, pageLinkArray, srsObjArr, numPagesPlaceholder)
+
+          // showSearchResults.showSearchResults(rows, genericHeaderObjGET, formInputsObjGET, searchResultsNonPag, srcRsCSV_nonPag, srcRsCSVrvw_nonPag,
+          //   edlpRows, nejRowsNonPagin)
+
+          showSearchResults.showSearchResults(rows, genericHeaderObjGET, formInputsObjGET, searchResultsPagGETarr, srcRsCSV_PagGETarr,
+            srcRsCSVrvwPagGETarr, edlpRows, nejRowsPagin)
+
+          res.render('vw-imwGenerator', {
+            title: `vw-imwGenerator from GET`,
+            srsObjArr: srsObjArr,
+            imwProductValObj: imwProductValObj,
+            imwProductArr: imwProductArr, //this is stringified product key/value pairs in an array to populate itemListAccumulatorPost
+            // objectifiedImwProdArr: objectifiedImwProdArr, //this is for DOM template display
+            tableName: tableName,
+            numQueryRes: numQueryRes,
+            pageLinkArray: pageLinkArray,
+            currentPage: currentPage,
+            numberOfPages: numPagesPlaceholder[0],
+            lastPage: numPagesPlaceholder[0] - 1,
+            firstPage: 0
+          })
+        })
+    }
+
+    queryEDI_Table_GET()
   })
 }
