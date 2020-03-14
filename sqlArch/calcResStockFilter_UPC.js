7878ad69e58f055f029cd6327f64ec8c8a4bae92
tsalm0n <tsalm0n@twc.com>
2020-03-14 03:11:49 -0400

    rough-in logic for stock filter

--- sqlArch/calcResStockFilter_UPC.js
+++ sqlArch/calcResStockFilter_UPC.js
@@ -14,6 +14,17 @@ module.exports = {
 
   calcResStockFilter_UPC: router.post('/calcResStockFilter_UPC', (req, res, next) => {
 
+    srcRsINDstocked = []
+    srcRsIND_NOTstocked = []
+    srcRsSMstocked = []
+    srcRsSM_NOTstocked = []
+    srcRsMTstocked = []
+    srcRsMT_NOTstocked = []
+    srcRsSHstocked = []
+    srcRsSH_NOTstocked = []
+    srcRsGLstocked = []
+    srcRsGL_NOTstocked = []
+
     searchResults = [] //clear searchResults from previous search
     console.log('calcResStockFilter_UPC says: searchResults from router.post level===>', searchResults)
     searchResultsForCSV = []
@@ -50,14 +61,9 @@ module.exports = {
     let storeNameArr = ['Indiana', 'Saint Matthews', 'Middletown', 'Springhurst', 'Gardiner Lane']
     let storeAbbrevArr = ['IND', 'SM', 'MT', 'SH', 'GL']
 
-
     function showSearchResults(rows) {
 
       let nhcrtRows = rows
-      // let edlpRows = rows[1] //targets 2nd query on rb_edlp_data table
-
-      console.log(`JSON.stringify(nhcrtRows[0])==> ${JSON.stringify(nhcrtRows[0])}`)
-      console.log(`nhcrtRows.length==> ${nhcrtRows.length}`)
 
       for (let i = 0; i < nhcrtRows.length; i++) {
         for (let j = 0; j < storeNameArr.length; j++) {
@@ -74,173 +80,54 @@ module.exports = {
                 nhcrtRows[i]['invLastsold'] > oneYearAgo ||
                 nhcrtRows[i]['invOnhand'] > 0) {
                 rsltsObj[`${storeAbbrev}_stocked`] = nhcrtRows[i]['invScanCode']
+                `srcRs${storeAbbrev}stocked`.push(rsltsObj)
               } else {
                 rsltsObj[`${storeAbbrev}_NOTstocked`] = nhcrtRows[i]['invScanCode']
+                `srcRs${storeAbbrev}_NOTstocked`.push(rsltsObj)
               }
-              searchResults.push(rsltsObj)
+              // searchResults.push(rsltsObj)
             }
           }
           calcResStockFilter_UPC(storeName, storeAbbrev)
         }
-
-        // calcResStockFilter_UPC('Indiana', 'IND')
-        // calcResStockFilter_UPC('Saint Matthews', 'SM')
-        // calcResStockFilter_UPC('Middletown', 'MT')
-        // calcResStockFilter_UPC('Springhurst', 'SH')
-        // calcResStockFilter_UPC('Gardiner Lane', 'GL')
       }
-
-
-      // for (let i = 0; i < nhcrtRows.length; i++) { //Add searched-for table entries from db to searchResults array, for
-      //   //displaying in the dynamic DOM table. Also add margin data, & retail & charm calcs to display in DOM table
-
-      //   function sfAud3(storeName, inStockCode) { //i.e. storeName = 'Indiana', inStockCode = 'IND'
-
-      //     let parsedInvVal = parseInt(nhcrtRows[i]['invOnhand'])
-
-      //     function sfAud3Results(shouldBeStockedOrNot) {
-      //       if (nhcrtRows[i]['stoName'] == storeName) {
-      //         let srcRsObj = {}
-      //         srcRsObj['ri_t0d'] = i
-      //         srcRsObj['invMismatchUPC'] = nhcrtRows[i]['invScanCode']
-      //         srcRsObj['invMismatchSKU'] = nhcrtRows[i]['ordSupplierStockNumber']
-      //         srcRsObj['invMismatchName'] = nhcrtRows[i]['invName']
-      //         srcRsObj['invMismatchStore'] = nhcrtRows[i]['stoName']
-      //         srcRsObj['invMismatchSFdata'] = nhcrtRows[i][inStockCode]
-      //         srcRsObj['invMismatchCPLTdata'] = nhcrtRows[i]['invOnhand']
-      //         srcRsObj['invMismatchLastRecd'] = nhcrtRows[i]['invLastreceived']
-      //         srcRsObj['invMismatchLastSold'] = nhcrtRows[i]['invLastsold']
-
-      //         srcRsObj['Comments2'] = shouldBeStockedOrNot
-
-      //         if (('2020-03-05' > nhcrtRows[i]['invLastreceived'] && nhcrtRows[i]['invLastreceived'] > '2019-12-05') ||
-      //           ('2020-03-05' > nhcrtRows[i]['invLastsold'] && nhcrtRows[i]['invLastsold'] > '2019-12-05')) {
-      //           srcRsObj['LastRecd0_3'] = nhcrtRows[i]['invLastreceived']
-      //           if (nhcrtRows[i][inStockCode] !== '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysStockedBut0_3'
-      //           }
-      //           if (nhcrtRows[i][inStockCode] == '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysNOTStockedBut0_3'
-      //             if (parsedInvVal > 0) {
-      //               srcRsObj['Comments1'] = 'SFsaysNOTStockedButInInv'
-      //             }
-      //           }
-      //         }
-      //         if (('2019-12-05' > nhcrtRows[i]['invLastreceived'] && nhcrtRows[i]['invLastreceived'] > '2019-09-05') ||
-      //           ('2019-12-05' > nhcrtRows[i]['invLastsold'] && nhcrtRows[i]['invLastsold'] > '2019-09-05')) {
-      //           srcRsObj['LastRecd3_6'] = nhcrtRows[i]['invLastreceived']
-      //           if (nhcrtRows[i][inStockCode] !== '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysStockedBut3_6'
-      //           }
-      //           if (nhcrtRows[i][inStockCode] == '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysNOTStockedBut3_6'
-      //             if (parsedInvVal > 0) {
-      //               srcRsObj['Comments1'] = 'SFsaysNOTStockedButInInv'
-      //             }
-      //           }
-      //         }
-      //         if (('2019-09-05' > nhcrtRows[i]['invLastreceived'] && nhcrtRows[i]['invLastreceived'] > '2019-06-05') ||
-      //           ('2019-09-05' > nhcrtRows[i]['invLastsold'] && nhcrtRows[i]['invLastsold'] > '2019-06-05')) {
-      //           srcRsObj['LastRecd6_9'] = nhcrtRows[i]['invLastreceived']
-      //           if (nhcrtRows[i][inStockCode] !== '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysStockedBut6_9'
-      //           }
-      //           if (nhcrtRows[i][inStockCode] == '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysNOTStockedBut6_9'
-      //             if (parsedInvVal > 0) {
-      //               srcRsObj['Comments1'] = 'SFsaysNOTStockedButInInv'
-      //             }
-      //           }
-      //         }
-      //         if (('2019-06-05' > nhcrtRows[i]['invLastreceived'] && nhcrtRows[i]['invLastreceived'] > '2019-03-05') ||
-      //           ('2019-06-05' > nhcrtRows[i]['invLastsold'] && nhcrtRows[i]['invLastsold'] > '2019-03-05')) {
-      //           srcRsObj['LastRecd9_12'] = nhcrtRows[i]['invLastreceived']
-      //           if (nhcrtRows[i][inStockCode] !== '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysStockedBut9_12'
-      //           }
-      //           if (nhcrtRows[i][inStockCode] == '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysNOTStockedBut9_12'
-      //             if (parsedInvVal > 0) {
-      //               srcRsObj['Comments1'] = 'SFsaysNOTStockedButInInv'
-      //             }
-      //           }
-      //         }
-      //         if (('2019-03-05' > nhcrtRows[i]['invLastreceived']) &&
-      //           ('2019-03-05' > nhcrtRows[i]['invLastsold'])) {
-      //           srcRsObj['LastRecd12plus'] = nhcrtRows[i]['invLastreceived']
-      //           if (nhcrtRows[i][inStockCode] !== '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysStockedBut12plus'
-      //           }
-      //           if (nhcrtRows[i][inStockCode] == '-') {
-      //             srcRsObj['Comments1'] = 'SFsaysNOTStockedBut12plus'
-      //             if (parsedInvVal > 0) {
-      //               srcRsObj['Comments1'] = 'SFsaysNOTStockedButInInv'
-      //             }
-      //           }
-      //         }
-      //         searchResults.push(srcRsObj)
-      //       }
-      //     }
-
-      //     if (nhcrtRows[i][inStockCode] == '-') {
-      //       if ((nhcrtRows[i]['invLastsold'] == '' || nhcrtRows[i]['invLastsold'] == undefined) &&
-      //         (nhcrtRows[i]['invLastreceived'] == '' || nhcrtRows[i]['invLastreceived'] == undefined)) {
-      //         sfAud3Results('NO LAST SOLD/RECD DATA')
-      //       }
-      //       if (nhcrtRows[i]['invLastsold'] > '2019-03-05') {
-      //         sfAud3Results('SHOULD_be_stocked')
-      //       }
-      //       if (nhcrtRows[i]['invLastreceived'] > '2019-03-05') {
-      //         sfAud3Results('SHOULD_be_stocked')
-      //       }
-      //       if (parsedInvVal > 0) {
-      //         sfAud3Results('SHOULD_be_stocked')
-      //       }
-
-
-      //     }
-
-      //     if (nhcrtRows[i][inStockCode] !== '-') {
-      //       if ((nhcrtRows[i]['invLastsold'] == '' || nhcrtRows[i]['invLastsold'] == undefined) &&
-      //         (nhcrtRows[i]['invLastreceived'] == '' || nhcrtRows[i]['invLastreceived'] == undefined)) {
-      //         sfAud3Results('NO LAST SOLD/RECD DATA')
-      //       } else {
-      //         if (nhcrtRows[i]['invLastsold'] < '2019-03-05' && nhcrtRows[i]['invLastreceived'] < '2019-03-05' && parsedInvVal <= 0) {
-      //           sfAud3Results('should_NOT_be_stocked')
-      //         }
-      //       }
-      //       // if (nhcrtRows[i]['invLastsold'] < '2019-03-05' && nhcrtRows[i]['invLastreceived'] < '2019-03-05' && parsedInvVal <= 0) {
-      //       //   sfAud3Results('should_NOT_be_stocked')
-      //       // }
-
-
-      //     }
-      //   }
-
-      //   sfAud3('Indiana', 'IND')
-      //   sfAud3('Saint Matthews', 'SM')
-      //   sfAud3('Middletown', 'MT')
-      //   sfAud3('Springhurst', 'SH')
-      //   sfAud3('Gardiner Lane', 'GL')
-
-      // }
       console.log(`JSON.stringify(searchResults)==> ${JSON.stringify(searchResults)}`)
     }
 
 
+    // function showSearchResults(rows) {
+
+    //   let nhcrtRows = rows
+
+    //   for (let i = 0; i < nhcrtRows.length; i++) {
+    //     for (let j = 0; j < storeNameArr.length; j++) {
+
+    //       storeName = storeNameArr[j]
+    //       storeAbbrev = storeAbbrevArr[j]
+
+    //       function calcResStockFilter_UPC(storeName, storeAbbrev) {
+    //         if (nhcrtRows[i]['stoName'] == storeName) {
+    //           let rsltsObj = {}
+    //           rsltsObj['ri_t0d'] = i
+    //           rsltsObj[`${storeAbbrev}_UPCs`] = nhcrtRows[i]['invScanCode']
+    //           if (nhcrtRows[i]['invLastreceived'] > oneYearAgo ||
+    //             nhcrtRows[i]['invLastsold'] > oneYearAgo ||
+    //             nhcrtRows[i]['invOnhand'] > 0) {
+    //             rsltsObj[`${storeAbbrev}_stocked`] = nhcrtRows[i]['invScanCode']
+    //           } else {
+    //             rsltsObj[`${storeAbbrev}_NOTstocked`] = nhcrtRows[i]['invScanCode']
+    //           }
+    //           searchResults.push(rsltsObj)
+    //         }
+    //       }
+    //       calcResStockFilter_UPC(storeName, storeAbbrev)
+    //     }
+    //   }
+    //   console.log(`JSON.stringify(searchResults)==> ${JSON.stringify(searchResults)}`)
+    // }
+
 
     function queryNhcrtTable() {
-      //v//retrieve info from database table to display in DOM table/////////////////////////////////////////////////////////
-      // connection.query(`SELECT * FROM ${formInput0} ORDER BY invLastreceived;`, function (err, rows, fields) {
-      //   if (err) throw err
-      //   showSearchResults(rows)
-
-      //   res.render('vw-nhcrtResults3', { //render searchResults to vw-MySqlTableHub page
-      //     title: 'sfAud3 (using nhcrt table)',
-      //     searchResRows: searchResults,
-      //     loadedSqlTbl: loadedSqlTbl
-      //   })
-      // })
       connection.query(`SELECT * FROM ${formInput0}`, function (err, rows, fields) {
         if (err) throw err
         showSearchResults(rows)
@@ -248,10 +135,20 @@ module.exports = {
         res.render('vw-stockFilter_UPC', { //render searchResults to vw-MySqlTableHub page
           title: 'stockFilter_UP (using nhcrt table)',
           searchResRows: searchResults,
-          loadedSqlTbl: loadedSqlTbl
+          loadedSqlTbl: loadedSqlTbl,
+          srcRsINDstocked: srcRsINDstocked,
+          srcRsIND_NOTstocked: srcRsIND_NOTstocked,
+          srcRsSMstocked: srcRsSMstocked,
+          srcRsSM_NOTstocked: srcRsSM_NOTstocked,
+          srcRsMTstocked: srcRsMTstocked,
+          srcRsMT_NOTstocked: srcRsMT_NOTstocked,
+          srcRsSHstocked: srcRsSHstocked,
+          srcRsSH_NOTstocked: srcRsSH_NOTstocked,
+          srcRsGLstocked: srcRsGLstocked,
+          srcRsGL_NOTstocked: srcRsGL_NOTstocked,
+
         })
       })
-
     }
 
     queryNhcrtTable()
