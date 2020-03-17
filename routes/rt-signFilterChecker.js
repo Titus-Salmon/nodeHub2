97b023fa5830d56d88f32d11ed24cf40b12f89be
tsalm0n <tsalm0n@twc.com>
2020-03-17 15:08:21 -0400

    cleanup

--- routes/rt-signFilterChecker.js
+++ routes/rt-signFilterChecker.js
@@ -21,40 +21,40 @@ const {
 const {
   nhcrtDisplay
 } = require('../sqlArch/nhcrtDisplay')
-const {
-  nhcrtEdiJoin
-} = require('../sqlArch/nhcrtEdiJoin')
+// const {
+//   nhcrtEdiJoin
+// } = require('../sqlArch/nhcrtEdiJoin')
 const {
   nhcrtInfraSF_Join
 } = require('../sqlArch/nhcrtInfraSF_Join')
 const {
   save2CSVreviewSfAud
 } = require('../sqlArch/save2CSVreviewSfAud')
-const {
-  save2CSVnhcrtEdiJoin
-} = require('../sqlArch/save2CSVnhcrtEdiJoin')
-const {
-  save2CSVreviewNEJ
-} = require('../sqlArch/save2CSVreviewNEJ')
-const {
-  saveIMW_CSV
-} = require('../sqlArch/saveIMW_CSV')
-const {
-  loadTable_MySqlHub
-} = require('../sqlArch/loadTable_MySqlHub')
-const {
-  calcResults
-} = require('../sqlArch/calcResults')
-const {
-  calcResultsGET
-} = require('../sqlArch/calcResultsGET')
+// const {
+//   save2CSVnhcrtEdiJoin
+// } = require('../sqlArch/save2CSVnhcrtEdiJoin')
+// const {
+//   save2CSVreviewNEJ
+// } = require('../sqlArch/save2CSVreviewNEJ')
+// const {
+//   saveIMW_CSV
+// } = require('../sqlArch/saveIMW_CSV')
+// const {
+//   loadTable_MySqlHub
+// } = require('../sqlArch/loadTable_MySqlHub')
+// const {
+//   calcResults
+// } = require('../sqlArch/calcResults')
+// const {
+//   calcResultsGET
+// } = require('../sqlArch/calcResultsGET')
 
-const {
-  calcResultsSfAud
-} = require('../sqlArch/calcResultsSfAud')
-const {
-  calcResultsSfAud2
-} = require('../sqlArch/calcResultsSfAud2')
+// const {
+//   calcResultsSfAud
+// } = require('../sqlArch/calcResultsSfAud')
+// const {
+//   calcResultsSfAud2
+// } = require('../sqlArch/calcResultsSfAud2')
 const {
   calcResultsSfAud3
 } = require('../sqlArch/calcResultsSfAud3')
@@ -79,19 +79,19 @@ router.post('/deleteRBtable', deleteRBtable)
 router.post('/populateRBtable', populateRBtable)
 router.post('/queryRBdb', queryRBdb)
 router.post('/nhcrtDisplay', nhcrtDisplay)
-router.post('/nhcrtEdiJoin', nhcrtEdiJoin)
+// router.post('/nhcrtEdiJoin', nhcrtEdiJoin)
 router.post('/nhcrtInfraSF_Join', nhcrtInfraSF_Join)
-router.post('/save2CSVnhcrtEdiJoin', save2CSVnhcrtEdiJoin)
-router.post('/save2CSVreviewSfAud', save2CSVreviewSfAud)
-router.post('/save2CSVreviewNEJ', save2CSVreviewNEJ)
-router.post('/saveIMW_CSV', saveIMW_CSV)
-router.post('/loadTable_MySqlHub', loadTable_MySqlHub)
-router.post('/calcResults', calcResults)
-router.get('/calcResults', calcResultsGET)
+// router.post('/save2CSVnhcrtEdiJoin', save2CSVnhcrtEdiJoin)
+// router.post('/save2CSVreviewSfAud', save2CSVreviewSfAud)
+// router.post('/save2CSVreviewNEJ', save2CSVreviewNEJ)
+// router.post('/saveIMW_CSV', saveIMW_CSV)
+// router.post('/loadTable_MySqlHub', loadTable_MySqlHub)
+// router.post('/calcResults', calcResults)
+// router.get('/calcResults', calcResultsGET)
 
 
-router.post('/calcResultsSfAud', calcResultsSfAud)
-router.post('/calcResultsSfAud2', calcResultsSfAud2)
+// router.post('/calcResultsSfAud', calcResultsSfAud)
+// router.post('/calcResultsSfAud2', calcResultsSfAud2)
 router.post('/calcResultsSfAud3', calcResultsSfAud3)
 router.post('/loadTable_signFilterChecker', loadTable_signFilterChecker)
 
