b70fe222e8cce23850919ab4ae794bf0f31a7517
tsalm0n <tsalm0n@twc.com>
2020-03-17 23:55:42 -0400

    begin save filter results to csv for stockfilter

--- sqlArch/save2CSVstockfilter.js
+++ sqlArch/save2CSVstockfilter.js
@@ -10,6 +10,7 @@ module.exports = {
 
     let csvDataPostparsed = JSON.parse(req.body['csvDataPost'])
     console.log(`csvDataPostparsed[0]==> ${csvDataPostparsed[0]}`)
+    console.log(`JSON.stringify(csvDataPostparsed[0])==> ${JSON.stringify(csvDataPostparsed[0])}`)
     console.log(`csvDataPostparsed[0][0]==> ${csvDataPostparsed[0][0]}`)
     console.log(`csvDataPostparsed[0][0]['ri_t0d']==> ${csvDataPostparsed[0][0]['ri_t0d']}`)
     console.log(`JSON.stringify(csvDataPostparsed[0][0])==> ${JSON.stringify(csvDataPostparsed[0][0])}`)
