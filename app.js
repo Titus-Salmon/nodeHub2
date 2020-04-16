46e01120604d1275c92e0e3e997170ee6eb8321d
tsalm0n <tsalm0n@twc.com>
2020-04-16 13:42:12 -0400

    add option for updating rb_inventory to stock filter

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const rainbowCatTableHubRouter = require('./routes/rt-rainbowCatTableHub') //t0d
 const imwGeneratorRouter = require('./routes/rt-imwGenerator') //t0d
 const signFilterCheckerRouter = require('./routes/rt-signFilterChecker') //t0d
 const stockFilter_UPCRouter = require('./routes/rt-stockFilter_UPC') //t0d
+const sfUPC_w_RBinvUpdaterRouter = require('./routes/rt-sfUPC_w_RBinvUpdater') //t0d
 const pcwGenRouter = require('./routes/rt-pcwGen') //t0d
 const pcwGenINFRARouter = require('./routes/rt-pcwGenINFRA') //t0d
 const compareUnequalListsRouter = require('./routes/rt-compareUnequalLists') //t0d
@@ -55,6 +56,7 @@ app.use('/rainbowCatTableHub', rainbowCatTableHubRouter) //t0d
 app.use('/imwGenerator', imwGeneratorRouter) //t0d
 app.use('/signFilterChecker', signFilterCheckerRouter) //t0d
 app.use('/stockFilter_UPC', stockFilter_UPCRouter) //t0d
+app.use('/sfUPC_w_RBinvUpdater', sfUPC_w_RBinvUpdaterRouter) //t0d
 app.use('/pcwGen', pcwGenRouter) //t0d
 app.use('/pcwGenINFRA', pcwGenINFRARouter) //t0d
 app.use('/compareUnequalLists', compareUnequalListsRouter) //t0d
