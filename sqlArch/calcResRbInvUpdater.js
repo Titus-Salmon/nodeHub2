e2a92666c240212b7aef073dc2d760c675c0556c
tsalm0n <tsalm0n@twc.com>
2020-04-22 22:40:07 -0400

    begin handling of wishlist items to ignore

--- sqlArch/calcResRbInvUpdater.js
+++ sqlArch/calcResRbInvUpdater.js
@@ -80,6 +80,7 @@ module.exports = {
     console.log(`todaysDateRaw2==> ${todaysDateRaw2}`)
     console.log(`todaysDateRaw2.getMonth()==> ${todaysDateRaw2.getMonth()}`)
     oneMonthAgoRaw_pre.setFullYear(todaysDateRaw2.getFullYear())
+    console.log(`Date.now()==> ${Date.now()}`)
 
     let oneMonthAgoRaw_iso = oneMonthAgoRaw_pre.toISOString()
     console.log(`oneMonthAgoRaw_iso==> ${oneMonthAgoRaw_iso}`)
