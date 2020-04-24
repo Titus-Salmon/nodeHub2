17e6c832bb43ec0ef2b7ea6025e243d613b0db45
tsalm0n <tsalm0n@twc.com>
2020-04-24 01:57:20 -0400

    begin rb_inventory update auditor

--- sqlArch/rbInvUpdateAudit.js
+++ sqlArch/rbInvUpdateAudit.js
@@ -105,32 +105,32 @@ module.exports = {
     FROM ${rbInvNEW} updated
     JOIN ${rbInvOLD} orig ON updated.inv_upc
     WHERE updated.inv_upc = orig.inv_upc
-    AND updated.inv_in_stock != orig.inv_in_stock
-    ORDER BY updated.inv_in_stock;
+    AND updated.inv_sm_stock != orig.inv_sm_stock
+    ORDER BY updated.inv_sm_stock;
     
     SELECT updated.inv_upc AS new_inv_upc, updated.inv_name AS new_inv_name, updated.inv_mt_stock AS new_inv_mt_stock,
     orig.inv_upc AS old_inv_upc, orig.inv_name AS old_inv_name, orig.inv_mt_stock AS old_inv_mt_stock
     FROM ${rbInvNEW} updated
     JOIN ${rbInvOLD} orig ON updated.inv_upc
     WHERE updated.inv_upc = orig.inv_upc
-    AND updated.inv_in_stock != orig.inv_in_stock
-    ORDER BY updated.inv_in_stock;
+    AND updated.inv_mt_stock != orig.inv_mt_stock
+    ORDER BY updated.inv_mt_stock;
     
     SELECT updated.inv_upc AS new_inv_upc, updated.inv_name AS new_inv_name, updated.inv_sh_stock AS new_inv_sh_stock,
     orig.inv_upc AS old_inv_upc, orig.inv_name AS old_inv_name, orig.inv_sh_stock AS old_inv_sh_stock
     FROM ${rbInvNEW} updated
     JOIN ${rbInvOLD} orig ON updated.inv_upc
     WHERE updated.inv_upc = orig.inv_upc
-    AND updated.inv_in_stock != orig.inv_in_stock
-    ORDER BY updated.inv_in_stock;
+    AND updated.inv_sh_stock != orig.inv_sh_stock
+    ORDER BY updated.inv_sh_stock;
     
     SELECT updated.inv_upc AS new_inv_upc, updated.inv_name AS new_inv_name, updated.inv_gl_stock AS new_inv_gl_stock,
     orig.inv_upc AS old_inv_upc, orig.inv_name AS old_inv_name, orig.inv_gl_stock AS old_inv_gl_stock
     FROM ${rbInvNEW} updated
     JOIN ${rbInvOLD} orig ON updated.inv_upc
     WHERE updated.inv_upc = orig.inv_upc
-    AND updated.inv_in_stock != orig.inv_in_stock
-    ORDER BY updated.inv_in_stock;`, function (err, rows, fields) {
+    AND updated.inv_gl_stock != orig.inv_gl_stock
+    ORDER BY updated.inv_gl_stock;`, function (err, rows, fields) {
       if (err) throw err
       // console.log(`rows.length==>${rows.length}`)
       // console.log('rows[0][0]==>', rows[0][0])
