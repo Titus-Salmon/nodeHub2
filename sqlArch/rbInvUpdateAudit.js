8f3ed8d610a4f4e3a907adbe8e836112d82f4f8c
tsalm0n <tsalm0n@twc.com>
2020-04-25 07:10:34 -0400

    begin rb_inventory update auditor

--- sqlArch/rbInvUpdateAudit.js
+++ sqlArch/rbInvUpdateAudit.js
@@ -57,13 +57,13 @@ module.exports = {
         rbInvJoinObj_ind['new_inv_upcIND'] = indRows[i]['new_inv_upc'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_ind['new_inv_nameIND'] = indRows[i]['new_inv_name'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         // rbInvJoinObj_ind['new_inv_in_stockIND'] = indRows[i]['new_inv_in_stock']
-        if (indRows[i]['new_inv_in_stock'] = '') {
+        if (indRows[i]['new_inv_in_stock'] = null) {
           rbInvJoinObj_ind['new_inv_in_stockIND'] = 'EMPTY'
         } else {
           rbInvJoinObj_ind['new_inv_in_stockIND'] = indRows[i]['new_inv_in_stock']
         }
         // rbInvJoinObj_ind['old_inv_in_stockIND'] = indRows[i]['old_inv_in_stock']
-        if (indRows[i]['old_inv_in_stock'] = '') {
+        if (indRows[i]['old_inv_in_stock'] = null) {
           rbInvJoinObj_ind['old_inv_in_stockIND'] == 'EMPTY'
         } else {
           rbInvJoinObj_ind['old_inv_in_stockIND'] = indRows[i]['old_inv_in_stock']
@@ -78,13 +78,13 @@ module.exports = {
         rbInvJoinObj_sm['new_inv_upcSM'] = smRows[i]['new_inv_upc'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_sm['new_inv_nameSM'] = smRows[i]['new_inv_name'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         // rbInvJoinObj_sm['new_inv_sm_stockSM'] = smRows[i]['new_inv_sm_stock']
-        if (smRows[i]['new_inv_sm_stock'] = '') {
+        if (smRows[i]['new_inv_sm_stock'] = null) {
           rbInvJoinObj_sm['new_inv_sm_stockSM'] = 'EMPTY'
         } else {
           rbInvJoinObj_sm['new_inv_sm_stockSM'] = smRows[i]['new_inv_sm_stock']
         }
         // rbInvJoinObj_sm['old_inv_sm_stockSM'] = smRows[i]['old_inv_sm_stock']
-        if (smRows[i]['old_inv_sm_stock'] = '') {
+        if (smRows[i]['old_inv_sm_stock'] = null) {
           rbInvJoinObj_sm['old_inv_sm_stockSM'] = 'EMPTY'
         } else {
           rbInvJoinObj_sm['old_inv_sm_stockSM'] = smRows[i]['old_inv_sm_stock']
@@ -99,13 +99,13 @@ module.exports = {
         rbInvJoinObj_mt['new_inv_upcMT'] = mtRows[i]['new_inv_upc'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_mt['new_inv_nameMT'] = mtRows[i]['new_inv_name'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         // rbInvJoinObj_mt['new_inv_mt_stockMT'] = mtRows[i]['new_inv_mt_stock']
-        if (mtRows[i]['new_inv_mt_stock'] = '') {
+        if (mtRows[i]['new_inv_mt_stock'] = null) {
           rbInvJoinObj_mt['new_inv_mt_stockMT'] = 'EMPTY'
         } else {
           rbInvJoinObj_mt['new_inv_mt_stockMT'] = mtRows[i]['new_inv_mt_stock']
         }
         // rbInvJoinObj_mt['old_inv_mt_stockMT'] = mtRows[i]['old_inv_mt_stock']
-        if (mtRows[i]['old_inv_mt_stock'] = '') {
+        if (mtRows[i]['old_inv_mt_stock'] = null) {
           rbInvJoinObj_mt['old_inv_mt_stockMT'] = 'EMPTY'
         } else {
           rbInvJoinObj_mt['old_inv_mt_stockMT'] = mtRows[i]['old_inv_mt_stock']
@@ -120,13 +120,13 @@ module.exports = {
         rbInvJoinObj_sh['new_inv_upcSH'] = shRows[i]['new_inv_upc'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_sh['new_inv_nameSH'] = shRows[i]['new_inv_name'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         // rbInvJoinObj_sh['new_inv_sh_stockSH'] = shRows[i]['new_inv_sh_stock']
-        if (shRows[i]['new_inv_sh_stock'] = '') {
+        if (shRows[i]['new_inv_sh_stock'] = null) {
           rbInvJoinObj_sh['new_inv_sh_stockSH'] = 'EMPTY'
         } else {
           rbInvJoinObj_sh['new_inv_sh_stockSH'] = shRows[i]['new_inv_sh_stock']
         }
         // rbInvJoinObj_sh['old_inv_sh_stockSH'] = shRows[i]['old_inv_sh_stock']
-        if (shRows[i]['old_inv_sh_stock'] = '') {
+        if (shRows[i]['old_inv_sh_stock'] = null) {
           rbInvJoinObj_sh['old_inv_sh_stockSH'] = 'EMPTY'
         } else {
           rbInvJoinObj_sh['old_inv_sh_stockSH'] = shRows[i]['old_inv_sh_stock']
@@ -141,13 +141,13 @@ module.exports = {
         rbInvJoinObj_gl['new_inv_upcGL'] = glRows[i]['new_inv_upc'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         rbInvJoinObj_gl['new_inv_nameGL'] = glRows[i]['new_inv_name'] //could use smRows[i], mtRows[i], etc. here, since they're all the same
         // rbInvJoinObj_gl['new_inv_gl_stockGL'] = glRows[i]['new_inv_gl_stock']
-        if (glRows[i]['new_inv_gl_stock'] = '') {
+        if (glRows[i]['new_inv_gl_stock'] = null) {
           rbInvJoinObj_gl['new_inv_gl_stockGL'] = 'EMPTY'
         } else {
           rbInvJoinObj_gl['new_inv_gl_stockGL'] = glRows[i]['new_inv_gl_stock']
         }
         // rbInvJoinObj_gl['old_inv_gl_stockGL'] = glRows[i]['old_inv_gl_stock']
-        if (glRows[i]['old_inv_gl_stock'] = '') {
+        if (glRows[i]['old_inv_gl_stock'] = null) {
           rbInvJoinObj_gl['old_inv_gl_stockGL'] = 'EMPTY'
         } else {
           rbInvJoinObj_gl['old_inv_gl_stockGL'] = glRows[i]['old_inv_gl_stock']
