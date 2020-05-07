d7b78a965e8bb9609283b1f25bdc4803581dab41
tsalm0n <tsalm0n@twc.com>
2020-05-07 12:36:58 -0400

    begin handling grocery price exceptions

--- funcLibT0d/showSearchResults.js
+++ funcLibT0d/showSearchResults.js
@@ -795,9 +795,7 @@ module.exports = {
 
     }
     console.log('showSearchResults says: searchResults.length from showSearchResults()==->', searchResults.length)
-    console.log('showSearchResults says: searchResults[0] from showSearchResults()==>', searchResults[0])
-    // console.log('showSearchResults says: searchResults from showSearchResults()==->', searchResults)
-    // console.log('showSearchResults says: searchResultsForCSV from showSearchResults()==>', searchResultsForCSV)
-    console.log('showSearchResults says: searchResultsForCSVreview[0] from showSearchResults()==>', searchResultsForCSVreview[0])
+    // console.log('showSearchResults says: searchResults[0] from showSearchResults()==>', searchResults[0])
+    // console.log('showSearchResults says: searchResultsForCSVreview[0] from showSearchResults()==>', searchResultsForCSVreview[0])
   }
 }
