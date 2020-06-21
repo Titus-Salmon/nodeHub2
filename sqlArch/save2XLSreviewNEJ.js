8d9965516cbd7e4ffe240d60f8d825db04e2d310
tsalm0n <tsalm0n@twc.com>
2020-06-21 02:07:27 -0400

    begin implementation of excel4node

--- sqlArch/save2XLSreviewNEJ.js
+++ sqlArch/save2XLSreviewNEJ.js
@@ -13,6 +13,7 @@ module.exports = {
     console.log(`typeof Object.keys(srcRsXLS_nonPag[0])==> ${typeof Object.keys(srcRsXLS_nonPag[0])}`)
     console.log(`Object.keys(srcRsXLS_nonPag[0])==> ${Object.keys(srcRsXLS_nonPag[0])}`)
     console.log(`JSON.stringify(srcRsXLS_nonPag[0])==> ${JSON.stringify(srcRsXLS_nonPag[0])}`)
+    console.log(`JSON.stringify(srcRsXLS_nonPag[3])==> ${JSON.stringify(srcRsXLS_nonPag[3])}`)
     console.log(`Object.keys(srcRsXLS_nonPag[0])[0]==> ${Object.keys(srcRsXLS_nonPag[0])[0]}`)
 
     for (let i = 0; i < Object.keys(srcRsXLS_nonPag[0]).length; i++) {
