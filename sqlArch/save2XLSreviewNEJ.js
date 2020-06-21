6262592e6dd2079b7534b4c0a281a43d12031562
tsalm0n <tsalm0n@twc.com>
2020-06-21 17:10:49 -0400

    begin implementation of excel4node

--- sqlArch/save2XLSreviewNEJ.js
+++ sqlArch/save2XLSreviewNEJ.js
@@ -48,7 +48,7 @@ module.exports = {
       fill: { // §18.8.20 fill (Fill)
         type: 'pattern', // Currently only 'pattern' is implemented. Non-implemented option is 'gradient'
         patternType: 'solid', //solid=t0d //§18.18.55 ST_PatternType (Pattern Type)
-        bgColor: '#0FF0', // HTML style hex value. defaults to black
+        bgColor: 'bright green', // HTML style hex value. defaults to black
         // fgColor: string // HTML style hex value. defaults to black.
       },
     })
