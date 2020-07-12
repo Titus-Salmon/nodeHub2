54a494ac6f99901c3b49a6cc7feb935158d696bb
tsalm0n <tsalm0n@twc.com>
2020-07-12 00:36:19 -0400

    begin handling rainbowcat_update_tracker table save to xlsx

---
+++ rbcatUpdateTracker/save2XLSrbcut.js
@@ -0,0 +1,178 @@
+const express = require('express')
+const router = express.Router()
+// const fs = require('fs')
+const xl = require('excel4node')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host: process.env.RB_HOST,
+  user: process.env.RB_USER,
+  password: process.env.RB_PW,
+  database: process.env.RB_DB,
+  multipleStatements: true //MUST HAVE to make more than 1 sql statement in a single query
+})
+
+module.exports = {
+
+  saveXLSrbcut: router.post('/saveXLSrbcut', (req, res, next) => {
+
+    //NOTE++++++++>>> rbCatUpdtTrkrDisplayArr is the original array that holds the collection of SearchResults objects {columnName: cellValue}
+    //HOWEVER, since the inherent order (from showSearchResults()) of these key:value pairs is NOT NECESSARILY the order we want to display them
+    //in the excel file, and also since there are additional key:value pairs from rbCatUpdtTrkrDisplayArr that we DON'T want to display, we
+    //selectively reorder and/or remove the key:value pairs from rbCatUpdtTrkrDisplayArr to form the rbCUTdisplArr_selectiveReordering array
+    //(WITHOUT modifying the original rbCatUpdtTrkrDisplayArr array).
+
+    var rbCUTdisplArr_selectiveReordering = []
+
+    for (let a = 0; a < rbCatUpdtTrkrDisplayArr.length; a++) {
+      let reorderedResObj = {}
+      // THE ORDER OF THE FOLLOWING OBJECT KEYS IS CRITICAL TO THE ORDER OF EXCEL COLUMNS
+      reorderedResObj['date'] = rbCatUpdtTrkrDisplayArr[a]['date']
+      reorderedResObj['edi_vendor_name'] = rbCatUpdtTrkrDisplayArr[a]['edi_vendor_name']
+      reorderedResObj['wsImw'] = rbCatUpdtTrkrDisplayArr[a]['wsImw']
+      reorderedResObj['rtlImw'] = rbCatUpdtTrkrDisplayArr[a]['rtlImw']
+      reorderedResObj['items_updtd_ws'] = rbCatUpdtTrkrDisplayArr[a]['items_updtd_ws']
+      reorderedResObj['items_updtd_rtl'] = rbCatUpdtTrkrDisplayArr[a]['items_updtd_rtl']
+      reorderedResObj['note1'] = rbCatUpdtTrkrDisplayArr[a]['note1']
+
+      rbCUTdisplArr_selectiveReordering.push(reorderedResObj)
+    }
+
+    console.log(`JSON.stringify(rbCUTdisplArr_selectiveReordering[0])==> ${JSON.stringify(rbCUTdisplArr_selectiveReordering[0])}`)
+
+
+    // Create a new instance of a Workbook class
+    var wb = new xl.Workbook()
+
+    // Add Worksheets to the workbook
+    var ws = wb.addWorksheet('Sheet 1')
+
+    var bodyStyle = wb.createStyle({
+      alignment: {
+        wrapText: false,
+        horizontal: 'center',
+      },
+      font: {
+        color: 'black',
+        size: 12,
+      },
+      // numberFormat: '$#,##0.00; ($#,##0.00); -',
+    })
+
+    var headerStyle = wb.createStyle({
+      alignment: {
+        wrapText: false,
+        horizontal: 'center',
+      },
+      font: {
+        color: 'black',
+        size: 14,
+        bold: true,
+
+      },
+      fill: { // §18.8.20 fill (Fill)
+        type: 'pattern', // Currently only 'pattern' is implemented. Non-implemented option is 'gradient'
+        patternType: 'solid', //solid=t0d //§18.18.55 ST_PatternType (Pattern Type)
+        bgColor: 'black', // HTML style hex value. defaults to black
+        fgColor: 'bright green' // HTML style hex value. defaults to black.
+      },
+    })
+
+    var charmHilite = wb.createStyle({
+      fill: { // §18.8.20 fill (Fill)
+        type: 'pattern', // Currently only 'pattern' is implemented. Non-implemented option is 'gradient'
+        patternType: 'solid', //solid=t0d //§18.18.55 ST_PatternType (Pattern Type)
+        bgColor: 'black', // HTML style hex value. defaults to black
+        fgColor: '#92D050' // HTML style hex value. defaults to black.
+      },
+    })
+
+    var ediPriceHilite = wb.createStyle({
+      fill: { // §18.8.20 fill (Fill)
+        type: 'pattern', // Currently only 'pattern' is implemented. Non-implemented option is 'gradient'
+        patternType: 'solid', //solid=t0d //§18.18.55 ST_PatternType (Pattern Type)
+        bgColor: 'black', // HTML style hex value. defaults to black
+        fgColor: '#93CDDD' // HTML style hex value. defaults to black.
+      },
+    })
+
+    var sibBasePriceHilite = wb.createStyle({
+      fill: { // §18.8.20 fill (Fill)
+        type: 'pattern', // Currently only 'pattern' is implemented. Non-implemented option is 'gradient'
+        patternType: 'solid', //solid=t0d //§18.18.55 ST_PatternType (Pattern Type)
+        bgColor: 'black', // HTML style hex value. defaults to black
+        fgColor: 'yellow' // HTML style hex value. defaults to black.
+      },
+    })
+
+    var invalidOupName = wb.createStyle({
+      fill: { // §18.8.20 fill (Fill)
+        type: 'pattern', // Currently only 'pattern' is implemented. Non-implemented option is 'gradient'
+        patternType: 'solid', //solid=t0d //§18.18.55 ST_PatternType (Pattern Type)
+        bgColor: 'black', // HTML style hex value. defaults to black
+        fgColor: 'red' // HTML style hex value. defaults to black.
+      },
+    })
+
+    for (let i = 0; i < Object.keys(rbCUTdisplArr_selectiveReordering[0]).length; i++) {
+
+      ws.cell(1, i + 1) //this targets "header" cells
+        .string(`${Object.keys(rbCUTdisplArr_selectiveReordering[0])[i]}`)
+        .style(headerStyle)
+
+      for (let j = 0; j < rbCUTdisplArr_selectiveReordering.length; j++) {
+        ws.cell(j + 2, i + 1)
+          .string(`${Object.values(rbCUTdisplArr_selectiveReordering[j])[i]}`)
+          .style(bodyStyle)
+        if (Object.keys(rbCUTdisplArr_selectiveReordering[0])[i] == 'charm') {
+          ws.cell(j + 2, i + 1).style(charmHilite)
+        }
+        if (Object.keys(rbCUTdisplArr_selectiveReordering[0])[i] == 'ediPrice') {
+          ws.cell(j + 2, i + 1).style(ediPriceHilite)
+        }
+        if (Object.keys(rbCUTdisplArr_selectiveReordering[0])[i] == 'sibBasePrice') {
+          ws.cell(j + 2, i + 1).style(sibBasePriceHilite)
+        }
+        if (Object.values(rbCUTdisplArr_selectiveReordering[j])[i] == 'invalid oupName') {
+          ws.cell(j + 2, i + 1).style(invalidOupName)
+        }
+      }
+    }
+
+
+    wb.write(`${process.cwd()}/public/csv/${req.body['xlsPost']}.xlxs`)
+
+    // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //v//Automatically add note to rainbowcat table that Retail Review has been generated//////////////////////////////////////
+    // let rtlRvwFilename = req.body['xlsPost']
+    // //here we are doing some js magic to extract the "ediName" from the Rtl Rvw name we're saving (nejTableNameRtlRvwYYYMMDD):
+    // // let regex1 = /(\d+)/g
+    // let vendorNameSplit1 = rtlRvwFilename.split('nej')
+    // let vendorNameSplit2 = vendorNameSplit1[1]
+    // let vendorNameSplit3 = vendorNameSplit2.toLowerCase().split('rtlrvw')
+    // let vendorName = vendorNameSplit3[0]
+    // let ediVendorName = `EDI-${vendorName.toUpperCase()}`
+    // console.log(`ediVendorName==> ${ediVendorName}`)
+
+    // function updateRbCat() {
+    //   connection.query(
+    //     `UPDATE rainbowcat SET RtlRvw = '${req.body['xlsPost']}.xlxs' WHERE ediName = '${ediVendorName}'`,
+    //     function (err, rows, fields) {
+    //       if (err) throw err
+    //       res.render('vw-MySqlTableHub', {
+    //         title: `<<${process.cwd()}/public/csv/${req.body['xlsPost']}.xlxs SAVED, and rainbowcat updated>>`
+    //       })
+    //     })
+    // }
+
+    // updateRbCat()
+    // //^//Automatically add note to rainbowcat table that Retail Review has been generated//////////////////////////////////////
+    // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    res.render('vw-rbCatUpdtTrkr', {
+      title: `<<${process.cwd()}/public/csv/${req.body['xlsPost']}.xlxs SAVED>>`
+    });
+
+  })
+}
