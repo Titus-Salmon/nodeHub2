f0dcc71c4c643eb7e4a42beb1f5d3929c29e0de4
tsalm0n <tsalm0n@twc.com>
2020-07-24 23:20:33 -0400

    troubleshoot STATUS_BREAKPOINT error in all browsers when trying to save nhcrt table for Kehe

--- sqlArch/save2CSVcreatePop.js
+++ sqlArch/save2CSVcreatePop.js
@@ -35,7 +35,7 @@ module.exports = {
       // "pi2Description", "pi3Description", "pi4Description", "invPowerField3", "invPowerField4"
       "ri_t0d", "invPK", "invCPK", "invScanCode", "invName", "ordSupplierStockNumber", "invSize", "invReceiptAlias", "posTimeStamp", "invDateCreated",
       "invEmpFkCreatedBy", "ordQuantityInOrderUnit", "oupName", "stoNumber", "brdName", "dptName", "dptNumber", "sibIdealMargin", "venCompanyname",
-      "invLastreceived", "invLastsold", "invLastcost", "sibBasePrice", "invOnhand", "invOnorder", "invIntransit", "pi1Description",
+      "invLastreceived", "invLastsold", "invLastcost", "sibBasePrice", "invOnhand", "invOnorder", "invIntransit", "invMemo", "pi1Description",
       "pi2Description", "pi3Description", "pi4Description", "invPowerField3", "invPowerField4"
     ]
 
