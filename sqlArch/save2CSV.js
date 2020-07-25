e143e6402d512ff98dc937652b4f2db4ce396078
tsalm0n <tsalm0n@twc.com>
2020-07-24 22:54:20 -0400

    troubleshoot STATUS_BREAKPOINT error in all browsers when trying to save nhcrt table for Kehe

--- sqlArch/save2CSV.js
+++ sqlArch/save2CSV.js
@@ -25,8 +25,12 @@ module.exports = {
         } = require('json2csv')
 
         const fields = [
+            // "ri_t0d", "invPK", "invCPK", "invScanCode", "invName", "ordSupplierStockNumber", "invSize", "invReceiptAlias", "posTimeStamp", "invDateCreated",
+            // "invEmpFkCreatedBy", "ordQuantityInOrderUnit", "oupName", "stoNumber", "stoName", "brdName", "dptName", "dptNumber", "sibIdealMargin", "venCompanyname",
+            // "invLastreceived", "invLastsold", "invLastcost", "sibBasePrice", "invOnhand", "invOnorder", "invIntransit", "invMemo", "pi1Description",
+            // "pi2Description", "pi3Description", "pi4Description", "invPowerField3", "invPowerField4"
             "ri_t0d", "invPK", "invCPK", "invScanCode", "invName", "ordSupplierStockNumber", "invSize", "invReceiptAlias", "posTimeStamp", "invDateCreated",
-            "invEmpFkCreatedBy", "ordQuantityInOrderUnit", "oupName", "stoNumber", "stoName", "brdName", "dptName", "dptNumber", "sibIdealMargin", "venCompanyname",
+            "invEmpFkCreatedBy", "ordQuantityInOrderUnit", "oupName", "stoNumber", "brdName", "dptName", "dptNumber", "sibIdealMargin", "venCompanyname",
             "invLastreceived", "invLastsold", "invLastcost", "sibBasePrice", "invOnhand", "invOnorder", "invIntransit", "invMemo", "pi1Description",
             "pi2Description", "pi3Description", "pi4Description", "invPowerField3", "invPowerField4"
         ]
