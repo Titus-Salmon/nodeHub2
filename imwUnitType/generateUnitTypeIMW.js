3be3be2dc2cdc5f44c2e46e871a443e22ef45cb6
tsalm0n <tsalm0n@twc.com>
2020-08-13 14:08:21 -0400

    start setup for handling _bulk types

--- imwUnitType/generateUnitTypeIMW.js
+++ imwUnitType/generateUnitTypeIMW.js
@@ -36,9 +36,10 @@ module.exports = {
 
       let displayRows = rows
       console.log(`displayRows[0]==> ${displayRows[0]}`)
-      for (let n = 0; n < Object.keys(displayRows).length; n++) {
-        console.log(`Object.keys(displayRows[${n}])==> ${Object.keys(displayRows[n])}`)
-      }
+      console.log(`Object.keys(displayRows[0])==> ${Object.keys(displayRows[0])}`)
+      // for (let n = 0; n < Object.keys(displayRows).length; n++) {
+      //   console.log(`Object.keys(displayRows[${n}])==> ${Object.keys(displayRows[n])}`)
+      // }
 
       for (let i = 0; i < displayRows.length; i++) {
 
