41d7bd362b98acc786403a925ef4afd294ad74eb
tsalm0n <tsalm0n@twc.com>
2020-09-11 19:58:35 -0400

    start profits-by-vendor module

--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -11,8 +11,56 @@ const connection = mysql.createConnection({
 
 const venProfArrCache = require('../nodeCacheStuff/cache1')
 
+const d3 = require('d3')
+const jsdom = require('jsdom')
+const fs = require('fs')
+
+// var htmlStub = `
+// <html>
+//   <head></head>
+//   <body>
+//     <div id="dataviz-container"></div>
+//     <script src="js/d3.v3.min.js"></script>
+//   </body>
+// </html>` // html file skull with a container div for the d3 dataviz
+
+
 module.exports = {
   venProf: router.post('/venProf', (req, res, next) => {
+
+    // // pass the html stub to jsDom
+    // jsdom.env({
+    //   features: {
+    //     QuerySelector: true
+    //   },
+    //   html: htmlStub,
+    //   done: function (errors, window) {
+    //     // process the html document, like if we were at client side
+    //     // code to generate the dataviz and process the resulting html file to be added here
+    //   }
+    // })
+
+    var el = window.document.querySelector('#dataviz-container')
+    var body = window.document.querySelector('body')
+
+    // append the svg to the container selector
+    d3.select(el)
+      .append('svg:svg')
+      .attr('width', 600).attr('height', 300)
+      .append('circle')
+      .attr('cx', 300).attr('cy', 150).attr('r', 30).attr('fill', '#26963c')
+
+    // save result in an html file
+    var svgsrc = window.document.innerHTML
+
+    // fs.writeFile('index.html', svgsrc, function (err) {
+    //   if (err) {
+    //     console.log('error saving document', err)
+    //   } else {
+    //     console.log('The file was saved, open index.html to see the result')
+    //     //do a res.render here
+    //   }
+    // })
 
     let vendorName = req.body['vendorNamePost']
     console.log(`vendorName==> ${vendorName}`)
