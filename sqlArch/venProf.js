b37393451d882eefad69de14b4a523e51b37c832
tsalm0n <tsalm0n@twc.com>
2020-09-11 22:08:02 -0400

    start profits-by-vendor module

--- sqlArch/venProf.js
+++ sqlArch/venProf.js
@@ -12,55 +12,61 @@ const connection = mysql.createConnection({
 const venProfArrCache = require('../nodeCacheStuff/cache1')
 
 const d3 = require('d3')
-// const jsdom = require('jsdom')
-// const fs = require('fs')
+const jsdom = require('jsdom')
+const fs = require('fs')
 
-// var htmlStub = `
-// <html>
-//   <head></head>
-//   <body>
-//     <div id="dataviz-container"></div>
-//     <script src="js/d3.v3.min.js"></script>
-//   </body>
-// </html>` // html file skull with a container div for the d3 dataviz
+var htmlStub = `<div id="dataviz-container"></div>` // html file skull with a container div for the d3 dataviz
 
 
 module.exports = {
   venProf: router.post('/venProf', (req, res, next) => {
 
-    // // pass the html stub to jsDom
-    // jsdom.env({
-    //   features: {
-    //     QuerySelector: true
-    //   },
-    //   html: htmlStub,
-    //   done: function (errors, window) {
-    //     // process the html document, like if we were at client side
-    //     // code to generate the dataviz and process the resulting html file to be added here
-    //   }
-    // })
+    jsdom.env({
+      features: {
+        QuerySelector: true
+      },
+      html: htmlStub,
+      done: function (errors, window) {
+        // this callback function pre-renders the dataviz inside the html document, then export result into a static file
 
-    var el = window.document.querySelector('#dataviz-container')
-    var body = window.document.querySelector('body')
+        var el = window.document.querySelector('#dataviz-container'),
+          body = window.document.querySelector('body'),
+          circleId = 'a2324' // say, this value was dynamically retrieved from some database
 
-    // append the svg to the container selector
-    d3.select(el)
-      .append('svg:svg')
-      .attr('width', 600).attr('height', 300)
-      .append('circle')
-      .attr('cx', 300).attr('cy', 150).attr('r', 30).attr('fill', '#26963c')
+        // generate the dataviz
+        d3.select(el)
+          .append('svg:svg')
+          .attr('width', 600)
+          .attr('height', 300)
+          .append('circle')
+          .attr('cx', 300)
+          .attr('cy', 150)
+          .attr('r', 30)
+          .attr('fill', '#26963c')
+          .attr('id', circleId) // say, this value was dynamically retrieved from some database
 
-    // save result in an html file
-    var svgsrc = window.document.innerHTML
+        // make the client-side script manipulate the circle at client side)
+        var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
 
-    // fs.writeFile('index.html', svgsrc, function (err) {
-    //   if (err) {
-    //     console.log('error saving document', err)
-    //   } else {
-    //     console.log('The file was saved, open index.html to see the result')
-    //     //do a res.render here
-    //   }
-    // })
+        d3.select(body)
+          .append('script')
+          .html(clientScript)
+
+        // save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
+        var svgsrc = window.document.innerHTML
+        fs.writeFile(process.cwd() + '/views/inserts/venProfResults.html', svgsrc, function (err) {
+          if (err) {
+            console.log('error saving document', err)
+          } else {
+            console.log('The file was saved!')
+            res.render('vw-venProf', {
+              title: `Monthly profits by vendor: ${vendorName}`,
+              venProfArr: venProfArr,
+            })
+          }
+        })
+      } // end jsDom done callback
+    })
 
     let vendorName = req.body['vendorNamePost']
     console.log(`vendorName==> ${vendorName}`)
@@ -95,10 +101,10 @@ module.exports = {
       // res.send(rows)
       displayvenProf(rows)
 
-      res.render('vw-venProf', {
-        title: `Monthly profits by vendor: ${vendorName}`,
-        venProfArr: venProfArr,
-      })
+      // res.render('vw-venProf', {
+      //   title: `Monthly profits by vendor: ${vendorName}`,
+      //   venProfArr: venProfArr,
+      // })
     })
 
   })
